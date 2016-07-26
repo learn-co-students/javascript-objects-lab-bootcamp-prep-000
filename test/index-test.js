17e8cda671439435fde8baa05bf68dc9bdd78569
Joseph Cutler <jc3652@trevor.org>
2016-07-26 15:23:39 -0400

    update tests to browser based (#2)
    * update tests to browser based
    
    * fix last commit
    
    * fixed toEqual
    
    * migrated to new test format
    
    * added mocha, removed inline mocha imports

---
+++ test/index-test.js
@@ -0,0 +1,52 @@
+/*global describe, it */
+
+it('defines a `recipes` object', function() {
+  expect(typeof recipes).toEqual('object')
+})
+
+describe('updateObjectWithKeyAndValue(object, key, value)', function() {
+  it('returns a clone of `object` by adding `key` and `value` (it is non-destructive)', function() {
+    var obj = { prop: 1 }
+
+    expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).to.eql({
+      prop: 1,
+      prop2: 2
+    })
+
+    expect(obj).to.eql({ prop: 1 })
+  })
+})
+
+describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
+  it('updates `object` with the given `key` and `value` (it is destructive)', function() {
+    var obj = { prop: 1 }
+
+    expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).to.eql({
+      prop: 1,
+      prop2: 2
+    })
+
+    expect(obj).to.eql({
+      prop: 1,
+      prop2: 2
+    })
+  })
+})
+
+describe('deleteFromObjectByKey(object, key)', function() {
+  it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
+    var obj = { prop: 1 }
+
+    expect(deleteFromObjectByKey(obj, 'prop')).to.eql({})
+    expect(obj).to.eql({ prop: 1 })
+  })
+})
+
+describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
+  it('deletes `key` from object and returns object', function() {
+    var obj = { prop: 1 }
+
+    expect(destructivelyDeleteFromObjectByKey(obj, 'prop')).to.eql({})
+    expect(obj).to.eql({})
+  })
+})
