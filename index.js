db936b7e74704c46e2267d94c23416302b663c26
Christopher Lanus <cqlanus@gmail.com>
2016-08-27 14:47:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
