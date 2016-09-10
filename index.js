499ee9b0d6261be311e2893b1d1b171658decd9f
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-09-10 12:16:43 +0000

    Done.

--- index.js
+++ index.js
@@ -18,3 +18,10 @@ obj[key] = value
 var newObj = Object.assign({}, obj)
   return newObj
 }
+
+function deleteFromObjectByKey(object, key) {
+  var obj = {prop: '1'}
+  delete obj[key]
+  var newObj = Object.assign({}, obj)
+  return newObj
+}
