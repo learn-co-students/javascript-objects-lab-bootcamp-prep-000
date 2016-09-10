2736a1aa67ff17f24d8e9f86f7095b0ad220e73f
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-09-10 11:56:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {
+  flour: "3 cups",
+  water: "1 cup",
+  eggs: "3"
+}
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+obj[key] = value
+var newObj = Object.assign({}, obj)
+  return newObj
+}
