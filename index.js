8ef466f6ad88ede570ba9bb891fee7138351df1f
Byron Injeeli <byroninjeeli@gmail.com>
2016-09-21 14:25:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = new Object({
+
+});
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var clone = new Object({});
+  clone = Object.assign({},object,{[key]:value});
+  return clone;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var clone = new Object({});
+  clone = Object.assign({}, object);
+  delete clone.object;
+  return clone;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
