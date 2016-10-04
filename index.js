140b70dbee05a4f1bb67eae37a6664395bf22fa9
Stan Aretinskiy <sr575sr@gmail.com>
2016-10-04 19:52:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+// first of all need to create an object
+
+var recipes = { noodles: "1/2pound" };
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject = object;
+  newObject[key] = value;
+  return newObject;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = object;
+  delete newObject.key;
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
