4d841607a2659d2b6cb8d198235ade27bfaa2dda
Nicholas Rotondo <nicholaslouis1994@gmail.com>
2016-10-15 15:16:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+
+var updateObjectWithKeyAndValue = function(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value) {
+  object[key] = value
+  return object
+}
+
+var deleteFromObjectByKey = function(object, key) {
+  var newObject = Object.assign({}, object, { [key]: 'value' })
+  delete newObject[key]
+  return newObject
+}
+
+var destructivelyDeleteFromObjectByKey = function(object, key) {
+  delete object[key]
+  return object
+}
