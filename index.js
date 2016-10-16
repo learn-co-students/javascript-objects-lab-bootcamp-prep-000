5694010505fc8977546eef2d525a5d44f641b032
Matthew Yack <mail2matthew@gmail.com>
2016-10-16 04:42:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey (object, key, value) {
+  var newObj = Object.assign({}, object, { [key]: value})
+  delete newObj[key]
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
