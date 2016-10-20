7b66d868f55d81a721fa3b649fe80dadc797471f
Ugljesa Ijacic <ugison@gmail.com>
2016-10-20 16:00:47 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObject = Object.assign({}, object);
+  const newObject = Object.assign({}, object);
   delete newObject[key];
   return newObject;
 }
