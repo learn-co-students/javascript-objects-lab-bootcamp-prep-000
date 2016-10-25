7a8f8314c8bfdb02974c83e7286ebf61fe776b48
Azekah Griffiths <azekah.griffiths@icloud.com>
2016-10-25 19:19:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-var recipes = {key:'value'}
+
+var recipes = { eggs: '1 cup', flour: '2 cups', sugar: '3 tbsps'};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({},object,{[key]: value})
+  return Object.assign({},object,{[key]:value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key]=value
-  return object
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({},object)
-  delete newObject.key
-  return newObject
+  var newObj = Object.assign({},object);
+  delete newObj[key];
+  return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
-  return object
+  delete object[key];
+  return object;
 }
