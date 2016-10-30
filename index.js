ce9377009dc64178e325fe55cf54eb945c3f8ca9
Tee Mak <bunth667@gmail.com>
2016-10-30 14:52:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = { food:    'pizza',
+                dinner:  'tacos'
+              };
+
+function updateObjectWithKeyAndValue(obj,key,value){
+  return Object.assign({}, obj, {[key]: value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
+  obj[key] = value;
+  return obj;
+}
+function deleteFromObjectByKey(obj,key){
+  var newObj = Object.assign({}, recipes);
+  delete newObj.key;
+  return newObj;
+}
+function destructivelyDeleteFromObjectByKey(obj,key){
+  delete obj[key];
+  return obj;
+}
