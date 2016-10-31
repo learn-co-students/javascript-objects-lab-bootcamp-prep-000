524c43f207596ff7af1e02dfa40db24774ad99ee
jason brown <j.grant.brown@gmail.com>
2016-10-31 03:07:36 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function deleteFromObjectByKey(object,key){
   delete object.key
   return object
 }
-function destructiveDeleteFromObjectByKey(object,key){
+function destructivelyDeleteFromObjectByKey(object,key){
   delete object.key
   return object
 
