85c02d450eb47c74310e1d1f83480976a5ee382a
jason brown <j.grant.brown@gmail.com>
2016-10-31 03:01:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var recipes ={};
 function updateObjectWithKeyAndValue(object,key,value){
-  
+  object[key]=value
+  return object
+}
+function destructivelyUpdateObjectWithKeyAndVAlue(object, key, value){
+
+}
+function delteFromObjectByKey(object,key){
+}
+function destructiveDeleteFromObjectByKey(object,key){
+
 }
