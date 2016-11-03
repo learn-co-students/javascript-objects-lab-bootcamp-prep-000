dc1d20c8ab4bfb39665a7e90163de422585edb4a
Jackson Hoang <jkhoang313@gmail.com>
2016-11-03 05:24:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {milk: "1/2 cup"}
+
+function updateObjectWithKeyAndValue(obj, k, v) {
+  var newObj = obj
+  newObj[k] = v
+  return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
