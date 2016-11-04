7dcc4ae901d901c9797be29c4904d9514ba03795
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-04 18:31:33 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ var recipes = {
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
+}
