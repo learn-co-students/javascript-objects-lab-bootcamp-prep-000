b3bc34faef4dd15d4f21ee403ae5a0d370e54c38
Elizabeth Hall <elizabethhall205@gmail.com>
2016-11-04 15:36:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var recipes = {
 }
 
-//returns a clone of `object` by adding `key` and `valu
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign( { ['key']: 'value' })
+  return Object.assign({}, object, { [key]: value })
 }
