cde829baee17feebd378fae15f5f84aa1651958e
James Sargant <shorinbonsai@gmail.com>
2016-11-20 20:07:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newO = Object.assign({}, object);
+  delete newO.key;
+  return newO;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
