80c0a3ffa8cd77546f1c66a560adeb3922cbc05a
Stefan Culafic <culaficstefan@gmail.com>
2016-11-20 15:47:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = new Object();
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object [key] = value;
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  var obj = Object.assign({}, object);
+  delete obj [key];
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object [key];
+  return object
+}
