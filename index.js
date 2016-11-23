ebce73d8ce586bd0908b26b3d401f11d4de9d2ee
Young Chul Lim <youngchullim@yahoo.com>
2016-11-23 15:57:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {
+
+};
+function updateObjectWithKeyAndValue(object,key,value) {
+  return Object.assign({}, object, {[key]: value})
+};
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object;
+}
+function deleteFromObjectByKey(object,key) {
+  delete object.key;
+  return recipes;
+}
+
+function destructivelyDeleteFromObjectByKey(object,key) {
+  delete object[key];
+  return recipes;
+}
