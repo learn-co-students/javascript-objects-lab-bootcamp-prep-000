34ae7ab5007d16c1e3e878c786aec15fd0de58a2
Clifford Harvey <deeharvey06@yahoo.com>
2016-12-12 07:03:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(object, key, value){
+   return Object.assign({ key: 2, prop: 1 }, { prop: 1, prop2: 2 })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+object[key] =value
+return object
+}
+function deleteFromObjectByKey(object, key){
+  delete object.key;
+  return object = {}
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object ={}
+}
