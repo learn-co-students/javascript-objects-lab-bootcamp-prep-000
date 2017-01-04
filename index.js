a7faf11e776ea08c42c9bccdd6aefbba18087b66
Esdras  Etrenne <esdras.pierre91@gmail.com>
2017-01-04 18:21:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {
+  first : "ingredient"
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object,{[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+    return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var NewObject = Object.assign({},object)
+    delete NewObject[key]
+      return NewObject
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+    return object
+}
