9991a72671ba6ac66f54b04ea935387d50963275
Gabriel Avinaz <admiraleyelash@gmail.com>
2017-01-05 05:25:30 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj =  Object.assign(object, [key]);
+  var newObj =  Object.assign({}, object);
+  delete newObj[key]
   return newObj;
 }
 
