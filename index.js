3b4b9259e3e22147c60831f3f6180b171c1edc56
Derrick Chu <dchu74@gmail.com>
2017-01-13 01:01:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-
-  object[key] = value
-
-
-return object
-}
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-
-  delete object.key;
-  return object
+  return Object.assign({}, object, {[key]:value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-
-object[key] = value
-return object;
-
+  object[key]=value
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
   return object;
 }
