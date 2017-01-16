282bda83891a61bc6af4ae578b64085bea7aa056
Banu Yilmaz <paksoybanu83@gmail.com>
2017-01-16 20:31:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = new Object();
+function updateObjectWithKeyAndValue(recipes, key, value){
+  return Object.assign({}, recipes, {[key]: value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+  recipes[key] = value;
+  return recipes;
+}
+function deleteFromObjectByKey(newObj, key){
+  var newObj = Object.assign({}, recipes);
+  delete newObj.key;
+  return newObj;
+}
+function destructivelyDeleteFromObjectByKey(recipes, key){
+  delete recipes[key];
+  return recipes;
+}
