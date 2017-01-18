0e18e3e7c1bec615cf114264189800b4d3c3d0f7
Andrea Graham <andrea.f.graham@gmail.com>
2017-01-18 21:00:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+
+  return object
+}
