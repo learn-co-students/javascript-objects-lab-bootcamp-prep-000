4be376cdabe03066c95650be88bf6cb41d35be7f
Yujun Sung <yujunsung@gmail.com>
2017-01-19 21:11:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+var recipes = new Object();
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+
+  return Object.assign({}, object, { [key]: value })
+
+
+  
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+  object[key] = value;
+   
+  return object;
+  
+}
+
+function deleteFromObjectByKey(object, key){
+
+  
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+  return object;
+  
+}
+
+
+
+
+
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
+
+  delete object[key];
+  return object;
+  
+}
