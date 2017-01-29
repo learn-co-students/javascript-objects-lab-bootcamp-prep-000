408af04c341c8dbab820656a7e02c329f51f5378
Winston Tokuhisa <tokuhisawinston@outlook.com>
2017-01-29 18:21:27 +0000

    Done.

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
+// results in a false positive
+// function deleteFromObjectByKey(object, key) {
+//   return Object.assign(object, [key])
+// }
+
 function deleteFromObjectByKey(object, key) {
-  return Object.assign(object, [key])
+    var newObject = Object.assign({}, object)
+    delete newObject.key
+
+  return newObject
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
