e13a5da9b4d9b632cc5a50e89f43a2a11c9d4289
zach weiss <station.of.peace@gmail.com>
2017-02-01 16:01:07 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function destructivelyDeleteFromObjectByKey(object, key) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete newObj[key]
-  return newObj
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
 }
