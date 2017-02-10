54693622a75622867474c4fc3271d4b4be080641
Penn Wu <phw.dental@gmail.com>
2017-02-10 12:34:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+var updateObjectWithKeyAndValue = function(object, key, value) {
+  return Object.assign ({}, object, {[key]: value})
+};
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value) {
+  object[key] = value;
+  return object
+};
+var deleteFromObjectByKey = function(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
+};
+var destructivelyDeleteFromObjectByKey = function(object, key) {
+  delete object[key];
+  return object
+}
+updateObjectWithKeyAndValue(object, key, value);
+destructivelyUpdateObjectWithKeyAndValue(object, key, value);
+deleteFromObjectByKey(object, key);
+destructivelyDeleteFromObjectByKey(object, key);
