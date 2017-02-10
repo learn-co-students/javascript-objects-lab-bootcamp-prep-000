ef2e715970356753acb5c842de0ca190d4029b5d
Mikael Airlangga <mikael.airlangga@gmail.com>
2017-02-10 08:14:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var new_object = Object.assign({}, object, {[key]: value});
+  return new_object;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var new_object = Object.assign({}, object);
+  delete object.key;
+  return new_object;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
