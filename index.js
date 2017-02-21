4d7409da8ff68382f2d7dafb46921fa3e53ce5c4
Diana Sentoso <dianah456@gmail.com>
2017-02-21 00:05:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = object;
+  newObj[key] = value;
+  return newObj;
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = object;
+  delete newObj.key;
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
