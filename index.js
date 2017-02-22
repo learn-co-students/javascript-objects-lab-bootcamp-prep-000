1fd27d175b9c01950c5edf0bd7b66b7c6357137b
Charles Jarju <charlesjarju@gmail.com>
2017-02-22 15:57:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {rice: '2kg', meat: '500g'}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+ var objClone = Object.assign({}, object)
+ delete objClone[key]
+ return objClone
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
