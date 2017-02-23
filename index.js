4084bfae7ad3647127abaac9f69bf276fb1dbdd0
Natalie Dean <ndean87@gmail.com>
2017-02-23 22:31:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = {
+  flour: "2 cups",
+  eggs: "1",
+  salt: "1 teaspoon",
+  powder: "2 teaspoons"
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
