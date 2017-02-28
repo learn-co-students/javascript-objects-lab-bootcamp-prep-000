1b9e22ea4208333ec63e194fb6772fe335f1a0ba
Juan Diaz <juandiaz9114@gmail.com>
2017-02-28 23:43:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var recipes = { quesadilla : 'cheese',
+              cereal: 'milk',
+}
+
+function updateObjectWithKeyAndValue(object, key, value)
+{
+return Object.assign({},object, {[key]:value});
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+object[key] = value;
+  return object;
+}
+
+function destructivelyDeleteFromObjectByKey (object,key)
+{
+  delete object.key;
+  return object;
+}
+
+function deleteFromObjectByKey (object, key)
+{
+  var newRecipe = Object.assign({}, object);
+  delete newRecipe.key;
+  return newRecipe;
+}
