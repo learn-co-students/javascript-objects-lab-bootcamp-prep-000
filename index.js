5e278fa6cef3ee94129023120270a7052852ca74
pia smith <pips@piasmith.com>
2017-03-05 05:54:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+
+
+var updateObjectWithKeyAndValue = function(object, key, value){
+  return Object.assign({}, object, { [key]: value});
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
+  object[key] = value
+  return object;
+}
+
+var deleteFromObjectByKey = function(object, key){
+  var newObj = Object.assign({}, object)
+  delete newObj.key // true
+  return newObj; // {}
+}
+
+var destructivelyDeleteFromObjectByKey = function(object, key){
+  delete object.key
+  return object;
+}
