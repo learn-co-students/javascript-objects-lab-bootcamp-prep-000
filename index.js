96bb240e7fefc7b7d5982238c11439f26975d357
Timothy O'Brien <timothy.p.obrien@gmail.com>
2017-03-07 19:48:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(recipes, key, value){
+  recipes[key] = value;
+  return recipes;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key] = value;
+  return recipes;
+}
+
+function deleteFromObjectByKey(recipes, key) {
+  var newRecipes = Object.assign({}, recipes);
+  delete newRecipes[key];
+  return newRecipes;
+}
+
+function destructivelyDeleteFromObjectByKey(recipes, key) {
+  delete recipes[key];
+  return recipes;
+}
