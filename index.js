840a3212460a35378d7b3b91b183958c358b2b7e
Will Sauer <will@heueso.com>
2017-03-11 20:57:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+  var recipes = {};
+
+  var updateObjectWithKeyAndValue = (object, key, value) =>
+  {
+    return Object.assign ({}, object, {[key]: value});
+  }
+
+  var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) =>
+  {
+    object[key] = value;
+    return object;
+
+  }
+
+var deleteFromObjectByKey = (object, key) =>
+{
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject
+}
+
+var destructivelyDeleteFromObjectByKey = (object, key) =>
+{
+  delete object[key];
+  return object
+}
