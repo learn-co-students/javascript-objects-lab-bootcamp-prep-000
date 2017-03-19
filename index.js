0e956ece29c96b8d0cd7f7ce9f2f4cdf8452a97a
SImon Christ <simonchrist@gmx.de>
2017-03-19 17:57:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object,{[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var tempObject  = Object.assign({},object)
+  delete tempObject[key]
+  return tempObject
+}
+function destructivelyDeleteFromObjectByKey(object,key) {
+  delete object[key]
+  return object
+}
