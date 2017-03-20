f8ec779cf30ce8515d19df86c55e47d0a4f11e98
Narcita Hamilton <majesticwaterfall11@yahoo.com>
2017-03-20 18:30:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {recipes}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object [key] = value
+   return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+ key.slice
+ return object
+ }
+
+  function deleteFromObjectByKey(object, key){
+   key.slice
+   return object
+ }
+
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+   object [key] = value;
+   return object;
+}
