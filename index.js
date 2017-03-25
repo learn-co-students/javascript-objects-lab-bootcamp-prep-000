d83faff85db8034389c9b9a9c1c4ef0bfc76269c
Mitchell White <white.mitchell.f@gmail.com>
2017-03-25 06:37:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key = value;
+}
+
+function deleteFromObjectByKey(object, key) {
+  return Object.assign({}, object,)
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
