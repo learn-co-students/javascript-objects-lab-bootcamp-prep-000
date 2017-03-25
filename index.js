b9f2ac122436ecb29f92b0c61ebb608de910b34e
Mitchell White <white.mitchell.f@gmail.com>
2017-03-25 06:41:05 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
