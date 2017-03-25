4fd387a8552b93b3dcb4a7d79c73d128b11a3d99
Jin Li <jinbl19@gmail.com>
2017-03-25 01:10:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  var objectCpy = Object.assign({}, object);
+  objectCpy[key] = value;
+  return objectCpy;
 }
 
 
