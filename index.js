1218582898eb48b1e25461be95c23a5daa56acde
Kwang Hoon Christian Ahn <kwanghoonahn@gmail.com>
2017-03-29 23:18:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]=value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object)
+  newObject
+  delete newObject[key]
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+   delete object[key]
+   return object
+}
