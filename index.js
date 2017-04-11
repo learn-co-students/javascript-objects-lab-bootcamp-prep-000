92851fae0cbff7a0d3bc7337b395652da726cecf
Richard Steinbach <steinbach.dick@gmail.com>
2017-04-11 18:51:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var recipes2 = Object.assign({}, object, {[key]: value})
+  return recipes2
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  var recipes2 = Object.assign({}, object)
+  delete recipes2[key]
+  return recipes2
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
