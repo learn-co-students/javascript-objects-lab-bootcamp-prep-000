2843a2fd631daee6e5904ffae001c056ee40462d
Dan Strong <dangaston@gmail.com>
2017-04-13 05:11:47 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+
+function deleteFromObjectByKey(object, key) {
+  var copy = Object.assign(object);
+  delete copy.key;
+  copy;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
