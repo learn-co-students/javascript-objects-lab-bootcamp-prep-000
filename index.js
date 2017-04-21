24466ac529215c1ae7270d3de92c09fa534e3b95
Mohamad Khayat <moek91@outlook.com>
2017-04-21 22:25:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-function objects(){
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key){
+  return object;
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+ delete [object.key]
+ return object;
 }
