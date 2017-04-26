6c97717d3900b8ad9ee8ebe03f4da83ae36cb041
Abraham Rudy <aberudy@gmail.com>
2017-04-26 22:54:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-var recipe = {key: 'element'}
+var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object.key = element)
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj = {prop: 1}, {prop2: 2})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+return obj
+}
+
+function deleteFromObjectByKey(obj, key) {
+  delete obj.key
+return obj
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj.key
+  return obj
 }
