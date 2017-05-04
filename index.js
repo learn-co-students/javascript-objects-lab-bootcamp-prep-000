aae45ff1511570b2aed6892b488766dc5caef326
Mandy Sparber <sparber.mandy@gmail.com>
2017-05-04 22:12:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {key:'value'};
+
+var updateObjectWithKeyAndValue = (object, key, value) => {
+  return Object.assign({}, object, {[key]: value})
+
+};
+
+var destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+  object[key] = value;
+  return object
+};
+
+var deleteFromObjectByKey = (object, key) => {
+  var newObject = Object.assign({}, object);
+  delete newObject.key
+  return object
+};
+
+var destructivelyDeleteFromObjectByKey = (object, key) => {
+  delete object.key
+  return object
+};
