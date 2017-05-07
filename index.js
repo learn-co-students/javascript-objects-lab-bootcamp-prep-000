8ef0bde19a1a7c9640b7e22f66304a2b27283b48
Tyler Dejesus <tylerdejesus76@yahoo.com>
2017-05-07 04:48:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  recipes = {prop: 1, prop2: 2}
+
+  return recipes;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+  object[key] = value;
+
+  return object;
+
+}
+
+function deleteFromObjectByKey(object, key){
+  var object2 = Object.assign({}, object);
+
+  delete object2.key;
+
+  return object2;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+
+  return object;
+}
