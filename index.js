9a506a54dab3c095336b8c75776e73d802e94351
Christian Oliva <coliva92@gmail.com>
2017-05-14 15:58:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, {[key]: value})
+}
+function deleteFromObjectByKey(object, key){
+  var newRecipe = Object.assign({}, object);
+  delete newRecipe.key;
+  return newRecipe;
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object;
+}
