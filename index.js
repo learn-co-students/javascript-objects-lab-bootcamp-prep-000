c95c048ced104853aab97e3cd9af9ba98d3643ab
Alan Guevara <alan@redpanda.cc>
2017-05-24 15:20:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(obj,k,v){
+  return Object.assign({},obj,{[k]:v})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj,k,v){
+  obj[k]=v
+  return obj
+}
+
+function deleteFromObjectByKey(obj,k){
+var newObj=Object.assign({},obj)
+delete newObj[k]
+return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(obj,k){
+delete obj[k]
+return obj
+}
