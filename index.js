4324879326a087330774147820a57339a2659335
Jennifer Jenkins <landon002000@yahoo.com>
2017-06-05 15:41:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {flour:"3 Cups"}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object,key){
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
