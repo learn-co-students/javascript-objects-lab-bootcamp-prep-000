dce9c9c3a58e525a2901cdd2fc329345859871d2
Yiming Jiang <yiminghere@gmail.com>
2017-06-06 05:46:46 +0000

    P

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var ret = Object.assign({}, object)
+  delete ret[key];
+  return ret;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
