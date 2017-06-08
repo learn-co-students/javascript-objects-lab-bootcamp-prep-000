e65df004671baa77d4a9753764de6934acd2414c
Amar Chatman <amarjerome@gmail.com>
2017-06-08 20:20:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  delete object.key
+  return object
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key
+  return object
+}
