f85468424c1daf139ee114d41e7422b235e5c76d
Charlie Rosen <dherzl@protonmail.com>
2017-06-08 15:53:45 +0000

    Done.

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
