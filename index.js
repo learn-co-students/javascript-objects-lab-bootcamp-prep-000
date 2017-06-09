1527f81380ced410bf6ac8f0628a2086db47aa5b
Cole Shapiro <colesayershapiro@gmail.com>
2017-06-09 19:01:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var object = { key: "value"};
+  var newObject = Object.assign({}, object);
+  delete newObject.key;
+  return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  var object = { key: "value"};
+  delete object.key;
+  return object;
+}
