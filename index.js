58c5c5c2b739a5a1ae3b84579eac3e814d793e13
Guesly  Germain <germain1211@outlook.com>
2017-06-11 17:33:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue (object, key, value) {
+    return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
+    object[key] = value
+    return object
+}
+function deleteFromObjectByKey (object, key, value){
+    delete object.Key
+    return object
+}
+
+function destructivelyDeleteFromObjectByKey (object, key, value){
+  delete object[key]
+  return object
+}
