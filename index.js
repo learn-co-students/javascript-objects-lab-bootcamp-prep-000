85fd9a51a2d929a3a1bc04a8968b0d7acedea3c4
Dasha Nikitina <dashadariallc@gmail.com>
2017-06-22 13:37:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {key: "value"};
+
+ function updateObjectWithKeyAndValue(recipes, key, value){
+return Object.assign({}, recipes, { [key]: value });
+ }
+
+ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+recipes[key] = value;
+   return recipes;
+ }
+
+ function deleteFromObjectByKey(recipes, key){
+var newRecipes = Object.assign({}, recipes);
+delete newRecipes.key;
+   return newRecipes;
+ }
+function destructivelyDeleteFromObjectByKey(recipes, key){
+delete recipes.key;
+  return recipes;
+}
