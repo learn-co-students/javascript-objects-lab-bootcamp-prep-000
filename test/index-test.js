41e669ce7afa20745fac69882b176a3b50b3090a
Shaw <shawwian@gmail.com>
2017-06-25 14:33:16 -0600

    Added more specific description of a function in index-test.js

--- test/index-test.js
+++ test/index-test.js
@@ -14,18 +14,22 @@ describe('Objects', function() {
         prop2: 2
       })
     })
-    
+
     it('it does not modify the original object, but rather returns a clone with the new data', function() {
       var obj = { prop: 1 }
-    
+
       updateObjectWithKeyAndValue(obj, 'prop2', 2)
 
       expect(obj['prop2']).toBe(undefined)
     })
   })
 
+// UPDATED TO SAY "AND RETURNS THE ENTIRE UPDATED OBJECT"
+// This is to avoid the confusion of simply creating a function that updates the object
+// but does not return any value. This makes it clear that the function must update
+// the object AND return the updated object
   describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
-    it('updates `object` with the given `key` and `value` (it is destructive)', function() {
+    it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
       var obj = { prop: 1 }
 
       expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
@@ -44,10 +48,10 @@ describe('Objects', function() {
     it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
       var obj = { prop: 1 }
       var newObj = deleteFromObjectByKey(obj, 'prop')
-      
+
       expect(newObj['prop']).toBe(undefined)
     })
-    
+
     it('does not modify the original object (it is non-destructive)', function() {
       var obj = { prop: 1 }
 
@@ -60,14 +64,14 @@ describe('Objects', function() {
     it('returns object without the delete key/value pair', function() {
       var obj = { prop: 1 }
       var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-      
+
       expect(newObj['prop']).toBe(undefined)
     })
-    
+
     it('modifies the original object', function() {
       var obj = { prop: 1 }
       var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-      
+
       expect(obj['prop']).toBe(undefined)
     })
   })
