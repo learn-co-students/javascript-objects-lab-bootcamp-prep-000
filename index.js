4e30dc1e8731f3cd6eed4e8899d25b33a5abee87
Chris Miller <chris.miller8823@gmail.com>
2017-06-27 01:33:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) { //non-destructive way to make clone of and update object
-  return Object.assign(object, { [key]:value} );
+  return Object.assign({}, object, { [key]:value} );
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { //destructive way to update object
