2fc5516e60f3b714ab9eb7dbd5558704a23e8bd6
Dylan Kershaw <dylankershaw@me.com>
2017-07-04 14:23:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var cloneObject = Object.assign(
+    {}, object, {
+      [key]: value
+    }
+  );
+  return cloneObject;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  newObj.key;
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
