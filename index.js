5435c570e6cbfe3f211ed30f3bbcac82430a56fc
Kelsey Ojima <kmojima@gmail.com>
2017-07-05 21:00:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = { key: 'value'}
+
+function updateObjectWithKeyAndValue(recipes, key, value){
+  return Object.assign(recipes, {[key] : value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+  recipes[key] = value;
+  return recipes
+}
+
+function deleteFromObjectByKey(recipes, key){
+  delete recipes.key;
+  return recipes
+}
+
+function destructivelyDeleteFromObjectByKey(recipes, key){
+  delete recipes[key];
+  return recipes
+}
