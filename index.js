6b17e0f66f0b889239c9d5f7edc128f91181db54
Joey Cho <cjy12182000@naver.com>
2017-07-11 22:43:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes = {
+  applejuice:"apple with water",
+  orangejuice:"orange with water",
+  herbrice:"rice with herb"
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newobj = Object.assign({}, object);
+  newobj[key]=value;
+  return newobj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newobj = Object.assign({}, object);
+  delete newobj[key]
+  return newobj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
