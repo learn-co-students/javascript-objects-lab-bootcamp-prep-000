7314b8a1877b712c8b3ecfed7dafc20e8d2d95c1
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-07-18 20:25:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var recipes =
+{
+  Food : 111
+}
+
+function updateObjectWithKeyAndValue(object, key, value)
+{
+return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key)
+{
+
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key)
+{
+  delete object[key]
+  return object
+}
