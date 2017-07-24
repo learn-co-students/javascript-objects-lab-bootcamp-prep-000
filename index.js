f7e9296c4001f4547fba9c87c35728f2d2ca5772
Araba Taylor <araba.taylor20@gmail.com>
2017-07-24 01:53:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = new Object();
+var updateObjectWithKeyAndValue = function(object,key,value){
+  return Object.assign({}, object, {[key]:value});
+}
+var destructivelyUpdateObjectWithKeyAndValue = function(object,key,value){
+   object[key] = value
+   return object
+}
+var deleteFromObjectByKey = function(object, key){
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
+}
+var destructivelyDeleteFromObjectByKey = function(object, key){
+  delete object[key];
+  return object;
+}
