6263b19e4ee5a58ae79d32b64a51f54989f678c6
John Heeg <fireman010203@gmail.com>
2017-07-30 22:55:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {
+  eggs: "2 eggs",
+  toast: "3 pieces"
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
