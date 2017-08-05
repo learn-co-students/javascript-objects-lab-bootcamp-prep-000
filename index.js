ffa4c9a38a5abff73a73af4828f4fa007ed79193
Jordan Storms <jcstorms25@gmail.com>
2017-08-05 01:52:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(obj, key, val){
+  var newObject = Object.assign(recipes, obj, {[key]:val})
+  return newObject
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, val){
+  return Object.assign(obj, {[key]:val})
+}
+
+function deleteFromObjectByKey(obj, key){
+  var newObject = Object.assign(recipes, obj)
+  delete newObject[key]
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key){
+  delete obj[key]
+  return obj
+}
