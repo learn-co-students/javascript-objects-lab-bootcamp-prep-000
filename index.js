7ae1b4c5f0717a5901da7925a4c760a25d42905b
William Eichhold <wjeichhold@gmail.com>
2017-08-09 17:25:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = { mexican: 'tacos' }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign( {object}, [key] );
+  delete newObject[key]
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key]
+}
