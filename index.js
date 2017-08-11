8d109ad81d58cbba90c9c7ee70293572ce4acd57
David Lowes <djlowes@gmail.com>
2017-08-11 20:16:14 +0000

    Done.

--- index.js
+++ index.js
@@ -5,24 +5,17 @@ return Object.assign({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-var newobj = Object.assign({}, object, {[key]: value})
-return newobj
+object[key] = value
+return object
 }
 
+function deleteFromObjectByKey(object, key) {
+var newObj = Object.assign({}, object)
+delete newObj[key]
+return newObj
+}
 
-/*
-describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
-  it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
-    var obj = { prop: 1 }
-
-    expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
-      prop: 1,
-      prop2: 2
-    })
-
-    expect(obj).toMatch({
-      prop: 1,
-      prop2: 2
-    })
-  })
-})
+function destructivelyDeleteFromObjectByKey(object, key) {
+delete object[key]
+return object
+}
