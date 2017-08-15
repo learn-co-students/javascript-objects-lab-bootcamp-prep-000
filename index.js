3c0235a6b9ac8df1b20ab49aa5fd14219ea63972
Johann Baptista <baptistajohann@gmail.com>
2017-08-15 18:05:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = new Object({eggs:'two eggs', butter: '1 tablespoon', flour: '1 cup'})
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
