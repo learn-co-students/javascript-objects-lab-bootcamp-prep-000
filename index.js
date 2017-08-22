fd66a5cc11978ca5d674c10c4216ebd91a3c61e6
zarli tun burrow <zarli1977@gmail.com>
2017-08-22 18:06:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {
+  flour: '1 cup'
+};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]: value})
+}
+
+function deleteFromObjectByKey(object, key) {
+  return delete object.key
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
