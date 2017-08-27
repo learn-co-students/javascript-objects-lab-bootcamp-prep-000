848475a21457d83d00b2e20fcd70fbfbc59caf3d
David Crowell <davidleecrowell@gmail.com>
2017-08-27 18:08:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {key: 'value'}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object;
+}
+function deleteFromObjectByKey(object, key){
+  var newRecipes = Object.assign({}, object)
+  delete newRecipes.key
+  return recipes;
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return recipes;
+}
