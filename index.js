1ec13367017973134070d9720515cc4d5211144b
Ursh Khan <urshkhan@gmail.com>
2017-08-30 06:58:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-var recipes = {}
+var recipes = {};
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object,
+   {[key]: value})
+};
 
-function updateObjectWithKeyAndValue(object, key, value)
-{
-  return Object.assign({}, object, { [key]: value})
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+};
 
-}
-
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
-{
-  object[key] = value
-  return object
-}
-
-
-function deleteFromObjectByKey(object, key){
-
-  const newObj = Object.assign({}, object)
-
-  delete newObj[key]
-
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj.key
   return newObj
-}
+};
 
-
-function destructivelyDeleteFromObjectByKey(object, key)
-{
-  delete object[key]
-  return object
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object;
 }
