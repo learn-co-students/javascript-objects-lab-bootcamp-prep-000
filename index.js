76c99e70e891bb7cc705f7726b66a916071a6a45
T Hinder <theresa.hindersinn@gmail.com>
2017-09-01 16:10:15 +0000

    Done.

--- index.js
+++ index.js
@@ -2,23 +2,21 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newObject = object[key]= value;
-  return object, newObject
-}
-
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign( object, {key: value})
+  return Object.assign( {}, object, {[key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key]= value
+  object[key]= value;
   return object
 }
-/*
-function deleteFromObjectByKey(object, key){
 
+function deleteFromObjectByKey(object, key){
+  var newObj= Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-
+  delete object[key];
+  return object;
 }
