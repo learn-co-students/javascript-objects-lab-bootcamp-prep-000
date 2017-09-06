206add751c5a42640d6c98d956beabdbe2491170
Robert W <robwash@gmail.com>
2017-09-06 01:26:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+// More Objects Lab
+
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+
+    return Object.assign({}, object, {[key]: value })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
+  object[key] = value
+    return object
+}
+function deleteFromObjectByKey(object, key){
+
+  var obj = { object: key}
+  var newObj = Object.assign({}, obj)
+
+  delete newObj.key
+    return newObj
+
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+
+  delete object[key]
+    return object
+
+}
