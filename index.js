217c01f2093a4785aa41f7a18c25ae4164ff849e
Franco Aiello <steed430@hotmail.com>
2017-09-10 11:14:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes ={}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  object[key]=value
+  return object
+}
+function updateObjectWithKeyAndValue(object,key,value){
+  return Object.assign({},object,{[key]:value})
+}
+function deleteFromObjectByKey(object, key){
+  return delete object.key
+
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
