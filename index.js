b5f8b196a57993d1f3a70b3d8e0d4e5915aa8fc8
Iain Olliver <iain.olliver@gmail.com>
2017-09-11 16:30:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = new Object()
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var new_object = Object.assign({},  object, { [key] : value } )
+  return new_object;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var new_object = Object.assign( {}, object )
+  delete new_object[key]
+  return new_object;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object;
+}
