6be2a4f0166ebbaffa8622719695e79ae2dc5f85
Luke Borsare <lborsare1@gmail.com>
2017-09-12 23:13:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object,key,value) {
+  return Object.assign({}, object, {[key] : value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object,key) {
+  var object2 = Object.assign({}, object)
+  delete object2[key]
+  return object2
+}
+function destructivelyDeleteFromObjectByKey(object,key) {
+  delete object[key]
+  return object
+}
