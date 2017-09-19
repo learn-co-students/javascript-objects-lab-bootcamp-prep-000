4c6be359c4e4febe1d7d2b806eb84a9061327d74
Andy Miller <amillerr@gmail.com>
2017-09-19 01:16:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes = {
+  'breakfast': 'oatmeal',
+  'lunch': 'chili',
+  'dinner': 'salad'
+}
+
+var updateObjectWithKeyAndValue = function(object, key, value) {
+  var newObject = Object.assign({}, object);
+  newObject[key] = value;
+  return newObject;
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+var deleteFromObjectByKey = function(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
+}
+
+var destructivelyDeleteFromObjectByKey = function(object, key) {
+  delete object[key];
+  return object;
+}
