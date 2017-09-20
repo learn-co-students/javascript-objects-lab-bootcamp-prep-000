96f43955d10aea9287065dbefa6a49843dd64a99
John Burnett <tuninator@gmail.com>
2017-09-20 12:44:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newRecipes = Object.assign({}, object)
+
+  newRecipes[key] = value
+
+  return newRecipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object)
+
+  delete newObject[key]
+
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
