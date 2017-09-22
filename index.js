b00e4a68c83c6c9491ff6704e7a3f54d5f01e2de
Jarrod Dyer <jarrod@gasganno.com>
2017-09-22 21:55:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {}
+
+//2
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+//3
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+//4
+function deleteFromObjectByKey(object, key) {
+  return Object.assign({}, delete object.key)
+}
+//5
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return object = delete object.key
+}
