a707aa2aaf0f0f6b3b3af01cca0a1509eaba8b2f
Rodrigo Fontes <fontesr2@gmail.com>
2017-09-25 18:28:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+
+  var obj = {};
+
+  Object.assign(obj, object);
+
+  obj[key] = value;
+
+  return obj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+  object[key] = value;
+
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+
+  var obj = {};
+
+  Object.assign(obj, object);
+
+  delete obj[key];
+
+  return obj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
