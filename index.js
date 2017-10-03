272fc03eec301810a8c532f82bb499716936122e
imendoza06 <imendoza9691@gmail.com>
2017-10-03 04:40:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 var recipes = {};
 
-function updateObjectWitKeyAndValue(object, key, value){
-  object.key = value
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
   return object
+
 }
-function updateObjectWitKeyAndValue(object, key, value){
-  return Object.assign({}, object, { [key]: value })
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({}, object, {[key]: value})
+  return newObj;
 }
+// function updateObjectWitKeyAndValue(object, key, value){
+  // return Object.assign({}, object, {[key]: value});
+// }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object)
-  delete newObj.key;
-  newObj;
+  delete newObj[key];
+  return newObj;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key;
+  delete object[key];
   return object;
 }
