ea75c1de65cf512e067c5ff618a2783ff416b57a
Shibani Mookerjee <shibani.mookerjee@gmail.com>
2017-10-06 19:01:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  //non-destructive
+  return Object.assign({}, object, {[key] : value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  //deletes key, non-destructive
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
