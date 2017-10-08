bf35baa03a7cc1952f0bcd833d6e6eb0f9eb94ab
Alara Dirik <alaradirik@gmail.com>
2017-10-08 08:42:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+   object[key] = value
+   return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var obj2 = delete object.key;
+  return obj2
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
