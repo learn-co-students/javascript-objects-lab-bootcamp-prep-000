98e2d3779f829ba775b9795b95e4a019c2274288
Samiur Rashid <samrashid@gmail.com>
2017-10-14 23:51:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {
+  italian: 'spaghetti'
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key]=value
+  return recipes
+}
+
+function destructivelyDeleteFromObjectByKey(recipes, key) {
+  delete recipes[key]
+  return recipes
+}
+
+function updateObjectWithKeyAndValue(recipes, key, value) {
+  return Object.assign({}, recipes, { [key]: value})
+}
+
+function deleteFromObjectByKey(recipes, key) {
+  var newObj = Object.assign({},recipes)
+  delete newObj[key]
+  return newObj
+}
