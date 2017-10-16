83700979db28fa089be8786ddb0cd717d8ff12a7
Hollie Lambert <hollielulambert@gmail.com>
2017-10-16 04:06:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {
+  'Toast': 'Bread',
+  'Pasta': 'Noodles',
+  'Cereal': 'Milk'
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key] : value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  return delete object.key
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key]
+}
