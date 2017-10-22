f80874e82c7b4538b282381b1102c960b9fd13b0
Brandon Chin <brandonchin@me.com>
2017-10-22 16:51:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, {[key]: value})
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+};
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
+};
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
