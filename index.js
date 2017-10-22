7e9d7142cd98843b9e81c68c6da1bee45f406b26
Ramesh <gurams@gmail.com>
2017-10-22 02:25:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  retun Object.assign({},object,{}[key]:value})
+  return Object.assign( {}, object, {[key]:value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return object[key]=value
+  object[key]=value
+  return object
 }
 function deleteFromObjectByKey(object, key){
-  var newObj= Object.assign({},object)
+  const newObj= Object.assign({},object)
   delete newObj[key]
   return newObj
 }
