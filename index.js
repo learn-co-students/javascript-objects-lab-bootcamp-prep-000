1f504a33e1e165915ef45b9a90fa4d724410f144
Dongjin Xu <dongjin.xu.jill@gmail.com>
2017-10-25 16:49:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{ [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key){
+  var obj = Object.assign({},object);
+  delete obj[key];
+  return obj;
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
