47865ca222deb6e7841e66e07b466b88fe952bdf
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 22:56:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-var recipes = new Object();
+// define 'recipes' object
+var recipes = {};
 
+// returns a clone of 'object' by adding 'key' and 'value' (non-destructive)
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]:value})
+  return Object.assign({}, object, {[key]: value});
 }
 
+// updates 'object' with the given 'key and 'value' (destructive)
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key]=value
-  return object
+  object[key] = value;
+  return object;
 }
 
+// deletes 'key' from a clone of object and returns the new object (non-destructive)
 function deleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({}, object)
-  newObject
-  delete newObject[key]
-  return newObject
+  var objClone = Object.assign({}, object);
+  delete objClone.key;
+  return objClone;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key){
-   delete object[key]
-   return object
+// deletes 'key' from object and returns object
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
 }
