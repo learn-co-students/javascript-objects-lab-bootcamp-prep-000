0004190bd5587517b336d3b23b09ea700fa4e91f
Josh Kramer <joshtkramer@yahoo.com>
2017-10-25 18:21:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var recipes = {};
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var obj = { prop: 1 };
+  return Object.assign({}, obj, {prop2: 2});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return obj;
+  }
+
+const obj = {prop: 1}
+
+destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+function deleteFromObjectByKey(object, key){
+var obj = { prop: 1 };
+var newObj = Object.assign({}, obj);
+newObj;
+delete newObj.prop;
+return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
+
+var object =  {prop: 1};
+
+destructivelyDeleteFromObjectByKey(object, 'prop');
