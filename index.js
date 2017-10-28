85b8a05c948885c9fc4902cd3276df172e8b474f
Daryl Concha <concha.daryl@gmail.com>
2017-10-28 01:36:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {
+  breakfast: "3 eggs",
+  lunch: "3 bean soup",
+  dinner: "meatloaf"
+}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
