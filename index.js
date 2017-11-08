6e0b8457a63c74f3ea9655e1eddcd08197b8cd2d
Michelle B <mjblevins7@gmail.com>
2017-11-08 13:03:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+var recipes = {
+  ingredient1: "bread",
+  ingredient2: "mild cheese",
+  ingredient3: "sharp cheese",
+  ingredient4: "butter",
+  ingredient5: "tomato",
+  ingredient6: "garlic",
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
