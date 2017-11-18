d5612f661707a6a83c475aae3c9c85c9b0b8af10
DeAndre Pulliam <deandre.pulliam@yahoo.com>
2017-11-18 17:24:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes={}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key]= value
+  return object
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+   Object.assign({}, object)
+   delete Object.key
+   return Object
+}
+
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key]
+  return object
+}
