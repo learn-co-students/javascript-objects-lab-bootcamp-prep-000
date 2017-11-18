612b55236d9e5c14b83f7453c3bbf6c3bee5774f
Wing Chak Chan <chanbryant1224@gmail.com>
2017-11-18 22:32:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+var recipes = {}
+function updateObjectWithKeyAndValue (object, key, value){return Object.assign({},object,{[key]:value})}
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value){ object[key] = value
+  return object}
+function destructivelyDeleteFromObjectByKey (object, key){delete object[key]
+return object}
+function deleteFromObjectByKey(obejct, key){return delete {}[key]}
