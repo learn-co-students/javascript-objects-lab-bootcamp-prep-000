6232dd24f126e65ca9377789c4f8ddb883e25559
Franky Cen <frankyxme@gmail.com>
2017-11-19 02:36:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, {[key]: value})
+}
+
+function deleteFromObjectByKey(object, key){
+  var obj = Object.assign({}, object)
+  delete obj.key
+  delete obj[key]
+  return obj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key
+  delete object[key]
+  return object
+}
