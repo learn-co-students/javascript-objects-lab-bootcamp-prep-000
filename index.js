8481324ddd8e78e4c98a5c1621ed0786ed835275
Allison Tzadok <allisontzadok@gmail.com>
2017-12-04 21:10:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {"cheese": "2 cups"};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+ var copy = Object.assign({},object);
+ delete copy[key];
+ return copy;
+}
+
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key];
+  return object;
+}
