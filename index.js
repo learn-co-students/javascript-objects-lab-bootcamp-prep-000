8fd7c3053b74206a177f5f9ebfa02daa1ca00054
Joseph C <jciv.coding@gmail.com>
2017-12-06 18:00:24 +0000

    Done.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
+  object[key] = value;
   return object;
 }
 
