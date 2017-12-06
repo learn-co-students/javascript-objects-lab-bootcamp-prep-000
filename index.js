1046fac22e29f01976b242a2b700724a5dbd98f4
Joseph C <jciv.coding@gmail.com>
2017-12-06 17:56:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {
+  ScrambledEggs: 'Eggs'
+};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  return Object.assign({}, -1)
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
