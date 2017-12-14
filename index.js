71657f7411da9d820176798c2caf1110cd7d1642
Nicole Eidi <nicoleeidi1@gmail.com>
2017-12-14 09:26:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+const recipes={
+  flower:"1"
+}
+
+function updateObjectWithKeyAndValue(obj,key,value){
+  var newObj= Object.assign({}, obj, { [key]: value })
+  return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
+    obj[key]=value
+    return obj
+}
+
+function deleteFromObjectByKey(obj,key){
+  var newObj= Object.assign({},obj)
+  delete newObj[key]
+  return newObj
+}
+function destructivelyDeleteFromObjectByKey(obj,key){
+  delete obj[key]
+  return obj
+}
