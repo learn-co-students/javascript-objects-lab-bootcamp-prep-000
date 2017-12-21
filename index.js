2b807114928e6a248fc76a19eb920e3982363a82
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-21 17:33:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key]
+  return obj
+}
