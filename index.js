6b3874c933cc796e971b60d732005ae71ae5e02f
winter mutt <sejongsan@hotmail.com>
2017-12-24 20:18:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-var recipes = new Object();
-
+var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, { [key]: value });
+  return Object.assign({}, object, { [key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object;
+  return Object.assign(object, { [key]: value});
 }
 
 function deleteFromObjectByKey(object, key){
