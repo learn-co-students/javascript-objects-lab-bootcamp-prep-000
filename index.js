21f8ba73348f6c7688c53d0b45ffdd020abe4421
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-04 20:03:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(recipes, key, value){
+ return Object.assign( {}, recipes, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes,key,value){
+  recipes[key]=value;
+  return recipes;
+}
+
+function deleteFromObjectByKey(recipes,key){
+
+  var newObj = Object.assign({},recipes)
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(recipes,key){
+  delete recipes[key];
+  return recipes;
+}
