09a9449f8a08ed5da8d483719e5dc09617fd060b
Virginia Herron <inadream@gmail.com>
2018-01-10 03:37:54 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key]=value;
   return object;
 }
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+function destructivelyDeleteFromObjectByKey (object, key) {
+  delete object[key];
+  return object;
+}
