8969e4693d4df0e7b05065f7670972bbc63f9090
Rosie Wang <rosiedwang@yahoo.com>
2018-01-10 21:53:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = ["eggs","milk"]
+function updateObjectWithKeyAndValue(object,key,value)
+{
+  
+}
+function updateObjectByKey(object,key)
+{
+  
+}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value)
+{
+  
+}
+function deleteFromObjectWithKeyAndValue(object,key,value)
+{
+  
+}
+function deleteFromObjectByKey(object,key)
+{
+  
+}
+function destructivelyDeleteFromObjectByKey(object,key)
+{
+  
+}
