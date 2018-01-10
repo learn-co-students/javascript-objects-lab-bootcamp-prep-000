294e691aab67d0a0429cfd6872fb32dd56bf7a51
Virginia Herron <inadream@gmail.com>
2018-01-10 02:57:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 var recipes = new Object ({eggs:3})
+function updateObjectWithKeyAndValue(object, key, value) {
+ return Object.assign({}, object, { [key]: value });
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]=value;
+  return object;
+}
