4eb7aed168b1d15cedb8d8ffa97cbb0f52fa76bf
Jennifer E <je@jenniferevenson.com>
2018-01-11 05:57:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  // non-destructive update 
+  return Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  // non-destructive delete
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
