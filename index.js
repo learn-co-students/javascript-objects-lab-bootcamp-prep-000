499700847b4ba1166474c6a95d14ce35949478a3
SDS 800 <jeremyhyman@gmail.com>
2018-01-14 04:42:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var recipes = {}
+ 
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+ 
+  return Object.assign({}, object,{ [key]: value } )
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+  object[key] = value
+ 
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  object = Object.assign({}, object)
+  return delete object.key;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+return delete object[key]
+
+}
