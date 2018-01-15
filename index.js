3cca77b6fcee7f26006d27437d0d821daa288670
Anne van Gorkom <anne@vangorkom.com>
2018-01-15 12:17:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return object, Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+}
