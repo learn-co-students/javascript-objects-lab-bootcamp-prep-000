705bef102f69e505f0d89d33b57a582e37bf09f7
Adam Bloss <adam.bloss17@gmail.com>
2018-01-21 16:49:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = Object.assign({}, object);
+  obj[key] = value
+  return obj
+}
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  var obj = delete object.key;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
