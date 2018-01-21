a76c5fe7a8c77f24b89027bbc451589088fa326e
Raymond Harvey <raymh2002@aol.com>
2018-01-21 03:13:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = new Object({});
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var clone = Object.assign({}, (object));
+  clone[(key)] = (value);
+  return clone;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+ (object)[key] = (value); 
+ return (object);
+}
+
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, (object));
+  delete clone[(key)];
+  return clone;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete (object)[key];
+  return (object);
+}
