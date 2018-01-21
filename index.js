6e03cd3455af430f0ea64a2a2611d2c8c198ee51
Vladimir D <ande.vladimir@gmail.com>
2018-01-21 16:18:24 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value });
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return Object.assign( object, {[key]: value} );
+  object[key] = value;
+  return object;
 }
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object); 
@@ -11,7 +12,6 @@ function deleteFromObjectByKey(object, key){
   return newObj;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  var newObj = Object.assign(object); 
-  delete newObj[key];
-  return newObj;
+ delete object[key]; 
+ return object;
 }
