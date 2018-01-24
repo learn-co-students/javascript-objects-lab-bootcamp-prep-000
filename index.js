5b3a8548315d9c0ec3f521d283462c2599a614e2
Dawn Carroll <dsc254@gmail.com>
2018-01-24 11:37:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = {
+  eggs: "2",
+  sugar: "100 grams"
+};
+
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
+
