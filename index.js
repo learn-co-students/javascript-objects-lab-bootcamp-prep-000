a36caa8ab69ad80afbeaeee0e9da7df4ad444996
James Montoya <jhenrymontoya@gmail.com>
2018-01-31 22:03:27 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ var recipes = {
   
 };
 function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
-};
+}
+function deleteFromObjectByKey(object, key) {
+  var newObject = delete object.key;
+  return newObject;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key];
+}
+
+
+
+
+
