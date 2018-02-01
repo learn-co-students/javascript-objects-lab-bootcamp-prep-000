0dc079701d22767b1e6a2c7e67f4ae4dd088ea12
arye dov eidelman <aryedov.e@gmail.com>
2018-02-01 20:08:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-recipes = {};
+var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value});
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
-  return Object;
+  return object;
 }
 function deleteFromObjectByKey(object, key){
-
+  var temp = Object.assign({}, object);
+  delete temp[key];
+  return temp;
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-
+  delete object[key];
+  return object;
 }
-
-
-
-// playlist = { Kanye: "Gold Digger" };
-//
-// function updatePlaylist(playlist, artistName, songTitle){
-//   return Object.assign({}, playlist, {[artistName]: songTitle});
-// }
-// function removeFromPlaylist(playlist, artistName){
-//   temp = Object.assign({}, playlist);
-//  delete temp[artistName];
-//  return temp;
-// }
