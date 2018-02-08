8d3a8912070b72ece2fa204fc114652c8bf4620c
Samson Awolaja <samson.awolaja@gmail.com>
2018-02-08 20:44:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+   object[key] = value;
+   return object
+}
+
+function updateObjectWithKeyAndValue(object,key,value) {
+  return Object.assign({}, object,{[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key ,value) {
+  object[key] = value;
+   return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
+
