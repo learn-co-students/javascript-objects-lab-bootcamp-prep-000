76c4861084ba2819f001a700e80b93190e8c3e6f
Mark Cruz <markchriscruz@yahoo.com>
2018-02-13 05:57:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject = Object.assign({}, object)
+  newObject[key] = value
+  return newObject
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object)
+  delete newObject[key]
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
