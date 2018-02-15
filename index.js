14fc9a871eb57ea119b737986182f9bf8458492f
Thomas Casady <tscasady@gmail.com>
2018-02-15 23:18:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function deleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
