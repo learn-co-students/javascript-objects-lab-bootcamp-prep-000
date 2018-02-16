217a828d499937f277acbcdba65c9a516812615d
Susanna Brumm <susanna.brumm@gmail.com>
2018-02-16 02:07:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-function Objects(obj) {
- var recipes = Object.assign({}, obj)
- return recipes
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = Object.assign({}, object, {[key]: value})
+  return obj
 }
 
-//function updateObjectWithKeyAndValue(object, key, value) {
-  
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  var n_object = Object.assign({}, object)
+  delete n_object[key]
+  return n_object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
+
+
+  
