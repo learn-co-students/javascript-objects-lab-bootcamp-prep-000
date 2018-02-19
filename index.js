684569e19e06bac51e8682bb9842749e73736f91
edb- C <cofresi.ethel@gmail.com>
2018-02-19 04:45:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
@@ -5,11 +6,21 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+=======
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+>>>>>>> 94d4e76676bad480b5b7d5bef634f3e12c5aef77
   object[key] = value;
   return object;
 }
 
 function deleteFromObjectByKey(object, key) {
+<<<<<<< HEAD
   var newObj = Object.assign({}, object);
   delete newObj.key;
   return newObj;
@@ -17,5 +28,14 @@ function deleteFromObjectByKey(object, key) {
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
+=======
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+>>>>>>> 94d4e76676bad480b5b7d5bef634f3e12c5aef77
   return object;
 }
