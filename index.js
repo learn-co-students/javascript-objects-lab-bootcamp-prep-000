e20b88ee9948d7dbaa4ffc2a07c642c0bf6fe449
Abel Quintero <abel.j.quintero@gmail.com>
2018-02-28 21:14:14 +0000

    Done.

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object);
-  delete newObj.key;
-  return object;
+  delete newObj[key];
+  return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key;
+  delete object[key];
   return object;
 }
