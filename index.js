199728c285fb45ed5913d6cd06b7f6f4ad7c2fbb
Reza H <rezahnamavar@gmail.com>
2018-03-05 20:11:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = new Object()
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},object, {[key]: value})
+  }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  Object.assign({},object)
+  return delete object.key
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key]
+}
