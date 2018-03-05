a759f5b4a2b45c1851d285a0f4f2ebcef1db0d73
David <dwd7290@yahoo.com>
2018-03-05 05:22:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+   return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var tmpObject = Object.assign({}, object);
+  return delete tmpObject[key];
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key];
+
+}
