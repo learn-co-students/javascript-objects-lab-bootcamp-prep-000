ba74cba7fd2c666f3e05d61a92b6e30130d18a93
Michael Roth <mick2090@gmail.com>
2018-03-06 00:04:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  
+  return Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+ 
+  return object;
+}
+
+
+function deleteFromObjectByKey(object, key){
+  var objClone = delete object.key;
+  return objClone;
+}
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
