20c7e5778108d3355fca18de4aec74b5a4139194
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-03-21 21:38:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var clone = Object.assign({}, object, {[key]: value})
+  return clone
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object = Object.assign(object, {[key]:value})
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var clone = Object.assign({}, object)
+  delete clone[key]
+  return clone
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
