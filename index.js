156e4b24f2f2628b00a5e92684899da43711f042
Taylor August Giles <augustgiles8@gmail.com>
2018-03-22 02:17:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-var recipes = {prop: 1};
+var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
-updateObjectWithKeyAndValue(recipes, "prop2", "2"); 
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  object[key] = value;
+  return object;
 }
-destrustivelyUpdateObjectWithKeyAndValue(recipes, "prop2", "2");
 
 function deleteFromObjectByKey(object, key) {
-  var newRecipes = Object.assign({}, object);
-  delete newRecipes[key];
-  return newRecipes
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
 }
-deleteFromObjectByKey(recipes, "prop2");
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
-  return object
+  return object;
 }
-destructivelyDeleteFromObjectByKey(recipes, "prop2")
