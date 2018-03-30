04f96c14df4b2bce6023c2025a7ef41eadeb7002
Nasser Issa <nasissa97@gmail.com>
2018-03-30 08:43:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object,key){
+  var newObject = Object.assign({})
+  
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  object[key] = undefined
+  return object
+}
