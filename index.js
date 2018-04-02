de8687339620918b92440081049f54be91c31e54
Sydnie T <sydnietom@gmail.com>
2018-04-02 00:00:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {
+  old: "quiche",
+  recent: "dumpling", 
+  classic: "waffle"
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
