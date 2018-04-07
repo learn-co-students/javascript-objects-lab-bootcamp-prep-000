e544e6ed624efe1bd37d4c564e22f5341147d728
Enecas <hypercolorm@gmail.com>
2018-04-07 07:15:54 +0000

    Done.

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
+  //Alternative: const newObject = Object.assign({}, object), delete newObject[key], return newObject
   //Deletes key from a clone of object and returns modified clone
   var newObject = delete object.key;
   return newObject;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
+  //Alternative: delete object[key], return object
   //Returns modified object after removing key
   return delete object[key];
 }
