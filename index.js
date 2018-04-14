66f3acb7a308d8c5067c077c136c0b50efd0ade0
Roigrosa <roigrosa88@gmail.com>
2018-04-14 06:24:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {}
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+
+return Object.assign({},object, {[key]: value})
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var object = Object.assign({}, object);
+  delete object[key]
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
