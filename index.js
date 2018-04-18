6b1177cc05ec8ebce1ac197d116ae94b65e48daa
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-18 02:21:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]:value});
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign
+}
