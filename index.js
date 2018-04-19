951404aef5ac21fa921f736e55d3a6a8de78a78b
NAVDEEP SIDHU <sidhu44@gmail.com>
2018-04-19 05:16:30 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function updateObjectWithKeyAndValue(object, key, value){
  
  function deleteFromObjectByKey(object, key){
      var newObj = Object.assign({}, object)
-     delete newObj.key
+     delete newObj[key]
      return newObj
  }
  
