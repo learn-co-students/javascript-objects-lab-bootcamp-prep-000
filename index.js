84741395eb7c384b3a12906b5e2fdf7e53021ae9
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-02 03:23:09 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
