c1c6225ab537aa1581a489d887f0be06a3cbcd2b
Emily Collins <c.emily.collins@gmail.com>
2018-05-09 04:18:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {
+  eggs : '2',
+  flour : '1 tbsp'
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign( {}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
