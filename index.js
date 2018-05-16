f084ae381a89cc4076cb2e645d9ab91ea78477e5
ECHE OJI <echyth@yahoo.com>
2018-05-16 12:35:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]: value});
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
