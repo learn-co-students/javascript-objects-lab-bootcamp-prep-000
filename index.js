672a34fa58c6f8a90cc7edfa533b45a189d250d7
Emily Wessel <ettl02@gmail.com>
2018-05-18 15:51:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var recipes = {}
+var recipes = {};
 
-function updateObjectWithKeyAndValue (object, key, value){
- return Object.assign({}, object, { [key]: value })
+function updateObjectWithKeyAndValue(object, key, value){
+ return Object.assign({}, object, { [key]: value });
 }
 
 
 function desctructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
+  object[key] = value;
   
-  return object 
+  return object ;
 }
 
 function deleteFromObjectByKey(object, key) {
-  const newObj = Object.assign({}, object)
+  const newObj = Object.assign({}, object);
 
-  delete newObj[key]
+  delete newObj[key];
 
-  return newObj
+  return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
+  delete object[key];
 
-  return object
+  return object;
 }
