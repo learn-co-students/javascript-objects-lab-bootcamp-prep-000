6814b97b7e3ff88ea5d1afaffa634653e1b57721
Michael Peterson <mik3wk@gmail.com>
2018-05-19 03:08:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+let recipes = {};
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value });
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key) {
+  var deleted = Object.assign({}, object);
+  delete deleted[key];
+  return deleted;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
