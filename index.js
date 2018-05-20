83963eedb5689477a4a129154e5b0da47c73eef1
Ashwin George Philip <ashwinphilip27@gmail.com>
2018-05-20 05:17:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {}
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key] : value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var recipes2 = Object.assign({}, recipes)
+  delete recipes2.key
+  return recipes2
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
