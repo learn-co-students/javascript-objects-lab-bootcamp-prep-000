9d25b7bf56abfbbc814438acab3a5ac54758c243
dabobmarley@hotmail.com <dabobmarley@hotmail.com>
2018-05-21 09:30:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var recipes = {pizza: 'tomato'}
+var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
 return Object.assign({}, object, { [key]: value })
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
+  object[key] = value;
+  return object;
 }
 
-function deleteFromObjectByKey(object, key) {
-var newObj = Object.assign({}, object);
-delete newObj.key;
-return newObj;
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+ return object;
 }
+function deleteFromObjectByKey(object, key) {
+var newObject = Object.assign({}, object);
+delete newObject[key];
+return newObject;
+}
+
