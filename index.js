facd750f118edc0cd816a2d4258207094b2f847a
Kacia Hosmer <kacia@coconutrobot.com>
2018-05-21 03:43:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(recipes, key, value) {
+  
+  return Object.assign({}, recipes, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  
+  recipes[key] = value;
+  return recipes;
+}
+
+function deleteFromObjectByKey(recipes, key) {
+  
+  var newObj = Object.assign({}, recipes);
+  return delete newObj[key];
+
+}
+
+function destructivelyDeleteFromObjectByKey(recipes, key) {
+  delete recipes[key];
+  return recipes;
+}
