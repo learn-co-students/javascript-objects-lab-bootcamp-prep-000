79f76fd004fc2ab5894d355649dc12f8d1cb13a7
Yohannes Abera <yohannesns@gmail.com>
2018-05-28 02:43:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {breakfast: 'cereal'}
+
+function updateObjectWithKeyAndValue(object,key,value){
+    return Object.assign({},object,{[key]:value})
+      }
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+
+  object[key] = value
+  return object
+
+
+}
+function deleteFromObjectByKey(object,key){
+return  delete Object[key]
+
+
+}
+function destructivelyDeleteFromObjectByKey(object,key){
+ delete object[key]
+ return object
+
+}
