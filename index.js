e37cfc0880582bead59dcd83f61153daec6d30a7
Kyle Spencer <spencerk2@vcu.edu>
2018-05-31 00:28:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(a, b, c) {return Object.assign({}, a, {[b]: c})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(a, b, c) {
+  a[b] = c; return a
+  }
+  
+function destructivelyDeleteFromObjectByKey(a, b) {
+  delete a[b]; return a
+  
+}
