ccc8163e545a5331c3eb8dced0e34bf06813405d
Shravani PL <pl.shravani@gmail.com>
2018-06-01 20:00:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object,key,value) {
+  
+}
+function updateObjectWithKeyAndValue(object,key,value) {
   Object.assign({},object,{key:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object,key) {
+  delete object.key
+  
+}
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key]
+}
+function destructivelyDeleteFromObjectByKey(object,key) {
+  
 }
