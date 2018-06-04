ddade5ecadd212bc7a89d7186d4fa699312b661f
Ian Hollander <ianholla@gmail.com>
2018-06-04 13:01:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {
+  tomatoSoup: ["tomatoes", "soup"],
+  scrambledEggs: ["eggs", "butter", "salt"]
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
