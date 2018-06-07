f9a4f454df3561995fba6eac14cc1a0ae5f1f2a0
heii70 <williamgao70@yahoo.com>
2018-06-07 01:50:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key] : value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = {};
+  Object.assign(newObject, object);
+  delete newObject[key];
+  return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
