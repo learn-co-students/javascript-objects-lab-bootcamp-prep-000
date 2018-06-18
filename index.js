cad05a8bbea22f0e8868e079d1d2b7eaf775d372
Denise Sison <tinyboxes@gmail.com>
2018-06-18 23:25:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var obj = { foo: 'bar' }
- 
-var newObj = Object.assign({}, obj)
- 
-newObj // { foo: 'bar' }
- 
-delete newObj.foo // true
- 
-newObj // {}
- 
-obj // { foo: 'bar' }
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
