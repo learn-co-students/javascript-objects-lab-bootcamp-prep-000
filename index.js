969fe34ad5c86f517a672bd420cba542c61f9900
Daniel Johnson <danielfrankjohnson@gmail.com>
2018-06-18 16:59:55 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   );
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
