0637654e50d66ca1fc17eaf71fa4769162c3c342
Umyippa <sywhitehouse@gmail.com>
2018-06-19 17:00:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {};
+
+var newRecipes = Object.assign({}, object);
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return ({}, object, key, value);
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return ({}, key, value);
+}
