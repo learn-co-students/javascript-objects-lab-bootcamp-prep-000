d23d2ad76a33717aa324e7e990f11bc2e33fa4e9
Alexandra Batrak <alexandrabatrak@gmail.com>
2018-06-20 17:54:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = { key : 'value'}
+
+function updateObjectWithKeyAndValue (recipes, key, value) {
+  return Object.assign({}, recipes, { [key] : value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (recipes, key, value){
+  recipes[key] = value
+  return recipes
+}
+
+function deleteFromObjectByKey (recipes, key) {
+  var newRecipe = Object.assign ({}, recipes)
+  delete newRecipe[key]
+  return newRecipe
+}
+
+function destructivelyDeleteFromObjectByKey (recipes, key){
+  delete recipes[key]
+  return recipes
+}
