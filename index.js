fab432becbcb1311fbddd309b3c9bdda3a310382
Maxime Sateur <maxime.sateur@gmail.com>
2018-06-22 13:11:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-var Object = {recipes}
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key,value){
+object[key]= value
+
+return object
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key] : value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]= value
+
+ return object
+}
+
+function deleteFromObjectByKey(object, key){
+  const newObject = Object.assign({}, object)
+
+  delete newObject[key]
+
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+
+  return object
+}
