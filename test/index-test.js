74c18d81a726934e2081f5417397f9597271c1ef
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-26 02:01:39 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -7,11 +7,12 @@ describe('Objects', function() {
 
   describe('updateObjectWithKeyAndValue(object, key, value)', function() {
     it('returns an object with the orignal key value pairs and the new key value pair', function() {
-      var obj = { prop: 1 }
+      var obj = { prop: 1, prop2: 2 }
 
-      expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+      expect(updateObjectWithKeyAndValue(obj, 'foo', 5)).toMatch({
         prop: 1,
-        prop2: 2
+        prop2: 2,
+        foo: 5
       })
     })
 
