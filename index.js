927fcfe49e3ae48e239359f2db7495b40f7a2790
Carie Pigeon <cariepigeon81@gmail.com>
2018-06-29 17:46:09 +0000

    Done.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ destructivelyUpdateObjectWithKeyAndValue(recipes, key, value);
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   return delete newObj["key"];
+  
 }
 deleteFromObjectByKey(object, 'key');
 
