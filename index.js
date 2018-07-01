75c084fb543bbe8a7dd95e7ed8291f5190d5f3fc
josh.e.stroud@gmail.com Stroud <josh.e.stroud@gmail.com>
2018-07-01 02:19:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  var newObj = Object.assign({}, obj)
+  newObj[key] = value;
+  return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key]
+  return obj
+}
