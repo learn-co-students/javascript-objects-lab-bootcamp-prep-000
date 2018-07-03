8f3d2815e8e1965460161e19550de8e9e3d7b49f
Valeria Ragonese <ribesnero80@gmail.com>
2018-07-03 05:02:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = {};
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+object[key] = value;
+return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = delete object.key;
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+
+  delete object[key];
+  return object
+
+  // var newObj = Object.assign({}, object)
+  // delete object.key;
+  // return object.key
+}
