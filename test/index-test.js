32a9b2bbc8e32b5c68f821c80b3279bbe8354d9c
Jay Singh <jeewandeep.singh@gmail.com>
2018-07-05 23:17:41 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -73,3 +73,24 @@ describe('Objects', function() {
   })
 
 })
+
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({},object)
+  delete newObject[key]
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object}
