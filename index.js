fb16c3bd51d153cdd7e5cf96744d8eb691f04c9b
Benjamin Lee <nycxbl@hotmail.com>
2018-07-06 19:34:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object;
-}
-
-function updateObjectWithKeyAndValue(object, key, value){
   var newObject = Object.assign({}, object, {[key]: value});
   return newObject;
 }
