e807d9a4ba4bc0ce5ef7da95cea863823cb9cc2e
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-09 12:35:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes ={cheese: "1 pound", bread: "2 slices"}
+function updateObjectWithKeyAndValue (object, key, value){
+    return Object.assign({},object, {[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]= value
+  return object
+}
+
+function deleteFromObjectByKey(object,key){
+  var newObject = Object.assign ({}, object)
+  delete newObject.key
+  return newObject
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey (object,key){
+  delete object.key
+  return object
+}
