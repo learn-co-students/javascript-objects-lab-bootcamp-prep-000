63676c257d57084a65838aed82a9d0476a912c20
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-09 12:41:47 +0000

    Done.

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object,key){
-  var newObject = Object.assign ({}, object)
-  delete newObject.key
+  var newObject = Object.assign ( {}, object)
+  delete newObject[key]
   return newObject
-  return object
 }
 
 function destructivelyDeleteFromObjectByKey (object,key){
-  delete object.key
+  delete object[key]
   return object
 }
