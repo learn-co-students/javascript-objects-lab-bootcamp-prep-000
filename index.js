9388bb24bc51a6e926a65bff0015eedce6318eb3
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-10 12:29:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {'plantain':1,
+              ' cheese': 2,
+              'salami': 2,
+              'eggs': 1}
+
+
+function updateObjectWithKeyAndValue(object,key,value){
+  object[key] = value
+  return recipes1
+}
