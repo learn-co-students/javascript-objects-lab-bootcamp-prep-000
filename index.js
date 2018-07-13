9f4380778cb13185b18f50fb85ca0ba0227c647a
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-07-13 19:40:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes= {};
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]=value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+ var object={key};
+ var newObject=Object.assign({},object)
+ return object
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  var object={key};
+  var newObject=Object.assign({},object)
+  delete newObject[key]
+  return object
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
