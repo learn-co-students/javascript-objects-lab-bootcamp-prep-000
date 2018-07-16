b91b4782033bd0d141237505730bbdb49b5b062a
rdmw691@gmail.com <rdmw691@gmail.com>
2018-07-16 18:27:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]:value})
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, recipes, {key:[value]})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key = value
+  return object
+}NOPE
