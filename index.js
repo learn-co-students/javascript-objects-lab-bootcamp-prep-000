139dec38ecc41a57eb9ce6bf5244a2976693db81
Madeline Celis <madelinebcelis@gmail.com>
2018-07-16 17:41:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = new Object();
+
+function updateObjectWithKeyAndValue(obj, key, value){
+  var newObj = Object.assign({}, obj);
+  newObj[key] = value;
+  return newObj;
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+};
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+};
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+};
