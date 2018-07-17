764da8f20946749a480f0013ea6fa7812367e595
Edward Castillo <edwardcastillo@me.com>
2018-07-17 00:12:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
-var recipes = {}
+var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value){
- 
-  return object;
-  
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
 }
 
-function updateObjectWithKeyAndValue(object, key, value){
-  
-  return Object.assign({}, object, { [key]: value });
-  
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
-  
 }
 
-function deleteFromObjectByKey(object, key){
-  
-  var newObj = Object.assign({}, object, key);
-  delete newObj[key];
-  return newObj;
-  
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object, key);
+  delete newObject[key];
+  return newObject;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key){
-  
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
-  
 }
