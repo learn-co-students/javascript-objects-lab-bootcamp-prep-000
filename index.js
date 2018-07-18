2cad557ff269fa3cf7c3da5f80ae0d8c33a30c14
Tiffany Choong <t.choong@yahoo.com>
2018-07-18 15:32:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+
 var recipes = new Object({cookies: 'flour, sugar, butter, water'});
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return object.assign({}, object,{[key]: value});
+  return Object.assign({}, object, {[key]: value});
 }
 
-updateObjectWithKeyAndValue(recipes, )
+updateObjectWithKeyAndValue(recipes, 'Baked Chicken', "chicken, seasoning");
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+destructivelyUpdateObjectWithKeyAndValue(recipes, Salsa, "tomatoes & other stuff");
