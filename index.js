81fe879ca70b41e3f376e1c53169b69696ff2214
Tesalon Felicien <felicien.tesalon@gmail.com>
2018-07-19 01:20:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var recipes ={}
+
+function updateObjectWithKeyAndValue (object, key, value){
+ return Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){ 
+   object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+delete object.key;
+}
+
+function destructivelyDeleteFromObjectByKey (object,key){
+ return delete object.key;
+}
+
+function deleteFromObjectByKey(object, key){
+return Object.assign(delete object.key)  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+delete object.key;
+return object;
+}
