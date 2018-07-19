030ba15cf288127a7e633efc7245cfcf89c702e0
Hannah Do <hannah.hj.do@gmail.com>
2018-07-19 00:57:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = { };
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var tempRecipes = Object.assign({}, object);
+  tempRecipes[key] = value;
+  return tempRecipes;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var tempRecipes = Object.assign({}, object);
+  delete tempRecipes[key];
+  return tempRecipes;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
+
+
