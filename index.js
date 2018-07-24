0ef3f2943e0fc631c65c96cfaf531b3f7ed31e1d
r.chimeg@gmail.com <r.chimeg@gmail.com>
2018-07-24 14:20:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {key: "0"};
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  var clone = Object.assign({}, object)
+  delete clone[key];
+  return clone;
+}
+function deleteFromObjectByKey(object, key){
+  return Object.assign({}, object[key])
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+ delete object[key]
+ return object
+}
