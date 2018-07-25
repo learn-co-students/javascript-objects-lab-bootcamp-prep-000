79d91c82c9d00b2248fe25a155a39d1274b81af4
anthonykumasaka@gmail.com Kumasaka <anthonykumasaka@gmail.com>
2018-07-25 20:34:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(object, key, value){
+    return Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value; 
+  return object; 
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object)
+  return delete newObj.key 
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  return delete object[key]; 
+  
+}
