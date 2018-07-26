6980f795bafa89214687d4d0d31008d2f17f053a
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 12:54:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]=value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var object = {key};
+  var newObject = Object.assign({}, object);
+  delete newObject.key;
+  return object;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
+
+
+
+
+//   describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
+//     it('returns object without the delete key/value pair', function() {
+//       var obj = { prop: 1 }
+//       var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+//       expect(newObj['prop']).toBe(undefined)
+//     })
+
+//     it('modifies the original object', function() {
+//       var obj = { prop: 1 }
+//       var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+//       expect(obj['prop']).toBe(undefined)
+//     })
+//   })
+
+// })
