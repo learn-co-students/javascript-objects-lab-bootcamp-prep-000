813cf8ecd7c76b2f14623b53bd11e28f4a4dc7a0
Nadia <30985274+Nadiafa@users.noreply.github.com>
2018-07-27 10:26:33 +0100

    Update index.js
    Clean index.js

--- index.js
+++ index.js
@@ -20,24 +20,3 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
 }
-
-
-
-
-//   describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
-//     it('returns object without the delete key/value pair', function() {
-//       var obj = { prop: 1 }
-//       var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-//       expect(newObj['prop']).toBe(undefined)
-//     })
-
-//     it('modifies the original object', function() {
-//       var obj = { prop: 1 }
-//       var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-//       expect(obj['prop']).toBe(undefined)
-//     })
-//   })
-
-// })
