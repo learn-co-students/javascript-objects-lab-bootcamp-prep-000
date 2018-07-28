10dad6de618e1375760b173ddc9f4d6749f276cc
Kyle Rudeen <krudeen@gmail.com>
2018-07-28 22:28:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var recipes = { }
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+ 
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({ }, object)
+  delete newObj[key];
+  return newObj;
+  
+}
+
+function deleteFromObjectByKey(object, key){
+  delete key.object;
+  return object;
+}
+
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
+    return object;
+}
+    
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object
+}
