890455a77e0bb1b366ed1316267f867129a8283e
Amir Safoev <amirsafoev0@gmail.com>
2018-07-28 14:16:45 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object,{[key]:value});
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
