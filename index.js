4bb9bf320877960a92df3ad732f0e5ebf355c28b
Hassan Malik <hbmalik88@gmail.com>
2018-07-28 17:05:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {calimari: "squid"};
+function updateObjectWithKeyAndValue (object, key, value) {
+  return Object.assign({}, object, {[key]:value});
+}
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey (object, key) {
+  var newobject = Object.assign({}, object)
+  delete newobject[key];
+  return newobject;
+}
+function destructivelyDeleteFromObjectByKey (object, key) {
+  delete object[key];
+  return object;
+}
