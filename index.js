d16c66f6370136d8fa430aa6b55ad232ce494556
Hank Liu <hanke.liu@gmail.com>
2018-08-02 01:39:18 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
  }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
+  object[key] = value;
   return object;
 }
 
