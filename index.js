1dfe14b82ce4c1756fa8d3c3cf6c202287d5f8a3
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-02 19:37:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]= value
+  return object
+}
