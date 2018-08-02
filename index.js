bffdcac1fcb00394f46773ef437200a77198d113
Albert Chun <albert.s.chun@gmail.com>
2018-08-02 04:30:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = { "pasta": "carbonara" };
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  return Object.assign({}, object[key]);
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
+
+
