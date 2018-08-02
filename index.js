f84f718e6df73199103be8f9fd2b90f0f30831cb
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-02 20:18:49 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key]= value
   return object
 }
+function deleteFromObjectByKey(object, key) {
+  var newRecipes = Object.assign({}, object)
+  delete newRecipes[key]
+  return recipes
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
