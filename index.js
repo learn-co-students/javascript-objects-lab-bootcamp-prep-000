4098d1de61931d595d0add4bf78c35e29707cbd1
Malik Glossop <malik.glossop@gmail.com>
2018-08-09 01:01:58 +0000

    Done.

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, {[key]:value})
 }
 function deleteFromObjectByKey(object, key) {
-  var objectClone = Object.assign({},object)
-  delete objectClone
-  return objectClone
+  var object2 = Object.assign({}, object)
+  delete object2[key]
+  return object2
 }
-
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
