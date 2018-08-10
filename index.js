39d6b819f1a37a37a801cbc78462603423f193c3
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-10 12:05:47 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ var recipes = {
 }
 
 /*
-HINT: You might find deleteFromObjectByKey to be a bit hard to write non-destructively. Think about how we learned to use Object.assign. 
+HINT: You might find deleteFromObjectByKey to be a bit hard to write non-destructively. Think about how we learned to use Object.assign. What happens if we do
+
+var obj = { foo: 'bar' }
+ 
+var newObj = Object.assign({}, obj)
+ 
+newObj // { foo: 'bar' }
+ 
+delete newObj.foo // true
+ 
+newObj // {}
+ 
+obj // { foo: 'bar' }
+
+Hmmmmm...
  */
  
  function deleteFromObjectByKey() {
