eec42d77af15e0a510d256b854d5efc0467f45cb
Ryan Ho <rhohoman@gmail.com>
2018-08-10 15:46:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object,key,value){
+  return Object.assign({},object,{[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object,key){
+  var newObject = delete object.key;
+  return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key];
+  return object;
+}
+
+/*
+describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
+    it('returns object without the delete key/value pair', function() {
+      var obj = { prop: 1 }
+      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+      expect(newObj['prop']).toBe(undefined)
+    })
+
+    it('modifies the original object', function() {
+      var obj = { prop: 1 }
+      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+      expect(obj['prop']).toBe(undefined)
+    })
+  })
+*/
