1e877c9b27ba8d3b9c391510a96504f9b0798860
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-10 12:27:36 +0000

    Done.

--- index.js
+++ index.js
@@ -27,17 +27,18 @@ var recipes = {
   key1: 'value1'
 }
 
+/*
 function updateObjectWithKeyAndValue(object, key, value) {
 //  returns an object with the orignal key value pairs and the new key value pair
   object[key] = value
   return object
 }
+ */
 
-/*
 function UpdateObjectWithKeyAndValue(object, key, value) {
 //  it does not modifythe original object, but rather returns a clone with the new data
 }
- */
+
  
  /*
  function deleteFromObjectByKey() {
