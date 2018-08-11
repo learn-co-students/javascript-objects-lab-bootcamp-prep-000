f026443429e7150dee8854d86dce8c662a3c6107
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-11 01:02:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 var recipes = {
-  key1: 'value1'
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign({}, object)
-  newObject[key] = value
-  return newObject
+  return Object.assign({}, object, { [key]: value})
 }
 
 function  destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
