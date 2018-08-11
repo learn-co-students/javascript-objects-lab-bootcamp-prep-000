8c759dc57c8b8d9a7d706eed254d21dd3d34c417
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-11 00:51:49 +0000

    Done.

--- index.js
+++ index.js
@@ -2,36 +2,24 @@ var recipes = {
   key1: 'value1'
 }
 
-// 1) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pair:
-
 function updateObjectWithKeyAndValue(object, key, value) {
-// it does not modifythe original object, but rather returns a clone with the new data:
   var newObject = Object.assign({}, object)
   newObject[key] = value
   return newObject
 }
 
 function  destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-// updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object:
   object[key] = value
   return object
 }
 
-// 4) Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non-destructive):
-
-// 5) Objects deleteFromObjectByKey(object, key) does not modify the original object (it is non-destructive):
-
 function deleteFromObjectByKey(object, key) {
-// does not modify the original object (it is non-destructive):
   var newObject = Object.assign({}, object)
   delete newObject[key]
   return newObject
 }
 
-// 7) Objects destructivelyDeleteFromObjectByKey(object, key) returns object without the delete key/value pair:
-
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key] 
   return object
 }
-
