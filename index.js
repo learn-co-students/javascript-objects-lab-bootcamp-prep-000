45510a99cf8518e6c9e7764aa9e9a26facdfefb5
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-11 00:35:31 +0000

    Done.

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ var recipes = {
   key1: 'value1'
 }
 
-/*
-function updateObjectWithKeyAndValue(object, key, value) {
-// returns an object with the orignal key value pairs and the new key value pair:
-  object[key] = value
-  return object
-}
- */
+// 1) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pair:
 
 // 2) Objects updateObjectWithKeyAndValue(object, key, value) it does not modifythe original object, but rather returns a clone with the new data:
 
-// 3) Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) updates `object` with the given `key` and `value` (it is destructive) and returns theentire updated object:
+// 3) Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object:
 
 // 4) Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non-destructive):
 
