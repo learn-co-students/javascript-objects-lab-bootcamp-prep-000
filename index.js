a74d2bcc9d10bfbf4bebf5d20b8b1b1e62d59814
ypeeyore2@yahoo.com <ypeeyore2@yahoo.com>
2018-08-11 01:07:15 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function  destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({}, object)
+  const newObject = Object.assign({}, object)
   delete newObject[key]
   return newObject
 }
