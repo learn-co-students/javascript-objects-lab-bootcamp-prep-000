45084a573bcc97109ef0eea009cb9e3ea8849db6
Lydia Thornton <lydia.d.thornton@gmail.com>
2018-08-12 22:42:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]: value});
+}
+
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign(object, {});
+  return delete clone['key'];
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object.key;
+}
