fb19704cca5083ab49d4af4a0645f7bd932cf8fa
Dario Spina <dario_spina01@live.it>
2018-08-14 19:36:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = new Object()
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+   object[key] = value
+   return object
+}
+function deleteFromObjectByKey(object, key){
+  var Newobject = Object.assign({}, object)
+  delete Newobject[key]
+  return Newobject
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
