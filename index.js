9b27d8f1b7eb9c8d020d376b89fa815eb4caa69f
eric.benedetto38@students.kbcc.cuny.edu <eric.benedetto38@students.kbcc.cuny.edu>
2018-08-15 16:11:05 +0000

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {key: value});
+  return Object.assign({}, object, {[key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value;
+  object[key] = value;
   return object;
 }
 
--- test/index-test.js
+++ test/index-test.js
@@ -8,31 +8,31 @@ describe('Objects', function() {
   // describe('updateObjectWithKeyAndValue(object, key, value)', function() {
   //   it('returns an object with the orignal key value pairs and the new key value pair', function() {
   //     var obj = { prop: 1 }
-  // 
+  //
   //     expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
   //       prop: 1,
   //       prop2: 2
   //     })
   //   })
-  // 
+  //
   //   it('it does not modify the original object, but rather returns a clone with the new data', function() {
   //     var obj = { prop: 1 }
-  // 
+  //
   //     updateObjectWithKeyAndValue(obj, 'prop2', 2)
-  // 
+  //
   //     expect(obj['prop2']).toBe(undefined)
   //   })
   // })
-  // 
+  //
   // describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
   //   it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
   //     var obj = { prop: 1 }
-  // 
+  //
   //     expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
   //       prop: 1,
   //       prop2: 2
   //     })
-  // 
+  //
   //     expect(obj).toMatch({
   //       prop: 1,
   //       prop2: 2
