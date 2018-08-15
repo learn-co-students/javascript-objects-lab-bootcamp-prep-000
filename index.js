10c0e5149294f5dfd19bb40e80883d42dc274cf5
Roundabout Rider <ntm.email@icloud.com>
2018-08-15 14:17:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = { object : 'key' }
+
+function updateObjectWithKeyAndValue(object, key, value ) {
+return recipes
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign({}, object) 
+  return recipes
+}
