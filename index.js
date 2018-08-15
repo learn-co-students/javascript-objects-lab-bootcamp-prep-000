17151560687c271ac7eb76a4ef84db0b8390aa4a
Ian Harrison <ianmartinharrison@outlook.com>
2018-08-15 11:27:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  object = Object.assign({}, object)
+  delete object[key]
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
