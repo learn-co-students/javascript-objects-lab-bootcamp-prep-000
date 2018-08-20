2e6c3344ec1f75ebca709312d24ab701966449de
Oliver de Sousa <oliver.seq@gmail.com>
2018-08-20 20:19:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-var recipes = {TomatoSoup: ["Tomato", "Carrots"]};
+var recipes = {prop: 1};
 
-function updateObjectWithKeyAndValue(){
-  return Object.assign(recipes, {TomatoSoup: "Onions"})
+function updateObjectWithKeyAndValue(recipes, prop){
+  return Object.assign({}, recipes, {prop2: 2})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, prop){
+  recipes.prop2 = 2;
+  return recipes
+}
+
+function deleteFromObjectByKey(recipes){
+  var newRecipes = {prop: 1};
+  delete newRecipes.prop;
+  return newRecipes
+}
+
+function destructivelyDeleteFromObjectByKey(recipes){
+  delete recipes.prop;
+  return recipes
 }
