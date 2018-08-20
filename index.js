a205968dd4160ed7abb281353c33cc3e8d4a9f81
V Echeveste <vaeb80@gmail.com>
2018-08-20 18:35:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(object, key, value){
+  var temp = Object.assign({}, object)
+  temp[key] = value
+  return temp
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  var temp = Object.assign({}, object)
+  delete temp[key]
+  return temp
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
