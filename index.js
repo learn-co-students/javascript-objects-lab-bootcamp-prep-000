41ad74144cf19d7982ef8c4fd9c27599f20cd6cb
Mark Ehler <ehlerm42@gmail.com>
2018-08-22 17:12:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+let recipes = []
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  let newobject = {}
+  newobject = Object.assign({}, object, {key, value});
+  return object
+}
+//it does not modifythe original object, but rather returns a clone with the new data
+// returns an object with the orignal key value pairs and the new key value pair:
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  let newObject = object
+  return newObject
+  }
+  //updates `object` with the given `key` and `value` (it is destructive) and returns theentire updated object:
+  //destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+
+
+function deleteFromObjectByKey(object, key) {
+  var newobj = Object.assign({}, object);
+  delete newobj[key]
+  return newobj
+}
+/*
+var obj = { foo: 'bar' }
+ 
+var newObj = Object.assign({}, obj)
+ 
+newObj // { foo: 'bar' }
+ 
+delete newObj.foo // true
+ 
+newObj // {}
+ 
+obj // { foo: 'bar' }
+*/
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+} 
+//returns object without the delete key/value pair:
+//modifies the original object:
