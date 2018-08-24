2a55ea9c3d3abe59116d65227b811cd112da8222
Gabrielle Bellini <gbellini90@gmail.com>
2018-08-24 23:04:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value) {
+return Object.assign({},object,{[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object [key] = value
+  return object
+}
+function deleteFromObjectByKey(object,key){
+ return delete object.key
+}
+function destructivelyDeleteFromObjectByKey(object,key){
+delete object[key]
+  return object
+}
