4b00aa5def582f00aa92849bfa3eb1882e319192
Sasha Hart <sashanicolehart@gmail.com>
2018-08-25 04:33:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
