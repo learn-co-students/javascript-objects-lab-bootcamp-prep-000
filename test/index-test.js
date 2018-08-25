1fc0cf6df89f3d103f76be339dfa9d25c2077be3
Chovin <Chovin@users.noreply.github.com>
2018-08-25 10:57:13 +1000

    removes 'it' from it() desc in tests

--- test/index-test.js
+++ test/index-test.js
@@ -14,8 +14,8 @@ describe('Objects', function() {
         prop2: 2
       })
     })
-    
-    it('it does not modify the original object, but rather returns a clone with the new data', function() {
+
+    it('does not modify the original object, but rather returns a clone with the new data', function() {
       var obj = { prop: 1 }
     
       updateObjectWithKeyAndValue(obj, 'prop2', 2)
