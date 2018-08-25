f8d41f4565e2585c64c953f5a11c847350f017e9
Sasha Hart <sashanicolehart@gmail.com>
2018-08-25 21:29:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
-}
-
-function updateObjectWithKeyAndValue(object, key, value) {
-  
+  var newObject = Object.assign( {}, object, { [key]: value } );
+  return newObject;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-  
-}
-
-function deleteFromObjectByKey(object, key) {
-  
+  var deletedObject = Object.assign( {}, object );
+  delete deletedObject[key];
+  return deletedObject;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+  delete object[key];
+  return object;
 }
