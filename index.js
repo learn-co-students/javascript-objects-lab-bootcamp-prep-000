d499b6785ed9516d6dfb5ebda7b0292d5208a553
Caren Rz <anruiz110@gmail.com>
2018-08-25 06:24:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+// anruiz23
+// Remember referencing keys in objects . or []
+// if passing strings use []
+
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object,key,value){
+  var obj2 = Object.assign({},object)
+  obj2[key] = value
+  return obj2
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var obj2 = Object.assign({},object)
+  delete obj2[key]
+  return obj2
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
