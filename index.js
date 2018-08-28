7ce47f65adb50190b69cff704f47e73058dbb27e
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-08-28 05:11:26 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-<<<<<<< HEAD
   return Object.assign(object, {[key] : value, [key] : value});
 }
 
@@ -20,11 +19,4 @@ function deleteFromObjectByKey(object, key) {
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
-}
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  return Object.assign(...object, key);
-=======
-  return Object.assign(...object, {[prop] : 2});
->>>>>>> 27dc146be137e5f5fdab942bd8c5f65956e9e65d
 }
