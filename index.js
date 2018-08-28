307db468a057b6fde1e6c2b72d1f73ddae422fa7
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-08-28 05:04:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+ return Object.assign(object, {[key] : value});
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key] : value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key] : value, [key] : value});
+}
+
+function deleteFromObjectByKey(object, key) {
+  return Object.assign({}, key); 
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return Object.assign(...object, key);
+}
