a8255ebf8d7d3324d41c431aa2504ff1ced5c1b4
Anthony Schons <amschons@gmail.com>
2018-08-28 17:18:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {Breakfast: 'eggs', Lunch: 'ham', Dinner: 'Steak'}
+
+function updateObjectWithKeyAndValue(recipes, key, value)
+{return obj.assign({}, recipes,{[prop2]:2})}
+
+function destructivelyUpdateObjectWithKeyAndValue (recipes breakfast) {["toast"]}
+
+function deleteFromObjectByKey
+
+function destructivelyDeleteFromObjectByKey delete recipes.Dinner
+
