bdeb4606de9ff5bfc6712d79772a90b02a7a4039
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-08-30 06:15:30 +0000

    Done.

--- index.js
+++ index.js
@@ -4,5 +4,17 @@ function updateObjectWithKeyAndValue (object,key,value) {
   return object
 }
 function updateObjectWithKeyAndValue (object, key, value) {
-   return object.assign({}, object, { [key]: value }) 
+   return Object.assign({}, object, { [key]: value }) 
+}
+function destructivelyUpdateObjectWithKeyAndValue (object,key,value) {
+ object[key] = value
+ return object
+}
+function deleteFromObjectByKey (object,key) {
+  delete object[key]
+  return object
+}
+function deleteFromObjectByKey (object,key) {
+  delete object.key;
+  return obj
 }
