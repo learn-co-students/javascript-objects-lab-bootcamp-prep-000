b84f6c68651190c451962484b26f2fd0c55b75d3
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-08-30 06:34:23 +0000

    Done.

--- index.js
+++ index.js
@@ -19,3 +19,7 @@ function deleteFromObjectByKey (object,key) {
   delete newObj[key]
   return newObj
 }
+function destructivelyDeleteFromObjectByKey (object,key) {
+  delete object[key]
+  return object
+}
