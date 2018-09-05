b2ea0f812fd75645c374bb3490bf021bcbe91617
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-09-05 00:58:10 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign({}, object, {key:[value]});
+  var newObject = Object.assign({}, object, {[key]:value});
   return newObject;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
+}
+
+function  destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
 }
