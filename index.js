69d63e6999cf76110a92b22243cbcf2c58a5aded
Eizik Gottesfeld <ezkmonsey@gmail.com>
2018-09-05 04:52:20 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue (object, key, value){
+  return Object.assign ({}, object, {[key]:value});
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
+  return Object.assign (object, {[key]:value});
+}
+
+function deleteFromObjectByKey(object, key, value){
+ 
+var newObj = Object.assign({}, object);
+ 
+delete newObj[key];
+ 
+return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key, value){
+  delete object[key];
+  return object
+}
+//correct
