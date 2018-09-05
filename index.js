cf3b76d6e499798c3a5af3821889a1eb8ddd1ff8
Jordan Williams <jordan@jordanchude.com>
2018-09-05 11:39:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(object, {[key]: value})
+}
+function deleteFromObjectByKey(object, key) {
+  var newRecipes = Object.assign({}, object)
+  delete newRecipes[key]
+  return newRecipes
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
