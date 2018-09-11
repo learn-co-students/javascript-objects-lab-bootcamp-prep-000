ec6290345f4d0d886cc8e8cf232d7a7178a38b87
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-11 16:28:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+var recipes = {
+  milk: '2 cups',
+  
+}
+
+function updateObjectWithKeyAndValue (object, key, value){
+   return Object.assign({}, object, {[key]: value});
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey (object, key) {
+  var clone = Object.assign({},object);
+  delete clone[key];
+  return clone;
+}
+
+function destructivelyDeleteFromObjectByKey (object, key) {
+  delete object[key];
+return object;
+}
+
+
+
+
+
+
