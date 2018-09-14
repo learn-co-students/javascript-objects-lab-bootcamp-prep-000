fcceb005955725bd02120f6cc494d72bbaa92d5e
Paul Clark <clark.paul123@gmail.com>
2018-09-14 22:01:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {
+  eggs: "good"
+};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object = Object.assign({}, object, {[key]: value})
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  object = Object.assign({}, object)
+  delete object[key]
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
