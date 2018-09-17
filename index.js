5b1f185a403577489e622cec4eb2927f6fa8d693
Virginia Dooley <vcdooley@gmail.com>
2018-09-17 15:32:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = { prop: 1 }
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = {prop: 1, prop2: 2}
+  return obj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  obj[key] = value
+  return obj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+ object[key] = value
+ return object
+}
+
+var obj = {foo: 'bar'}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, obj)
+  newObj
+  delete newObj.key
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  object[key] = value
+ return object
+}
