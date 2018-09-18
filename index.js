0d892dc39ea9c672c6a866677655af95edd5399b
Ana Bebic <anabebic.x@gmail.com>
2018-09-18 19:37:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-var recipes= {};
-
-function updateObjectWithKeyAndValue(obj, key, value) {
- 
-  return Object.assign({}, obj, { [key]: value })
-};
-
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value
- 
-  return obj
-};
-
-function deleteFromObjectByKey(object, key){
- var newObj = Object.assign({}, object)
-  delete newObj.key
+var recipes = new Object();
+ function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
+}
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ object[key] = value
+return object
+}
+ function deleteFromObjectByKey(object, key){
+  var newObj =  Object.assign({},object)
+  delete newObj[key]
   return newObj
-};
-
-function destructivelyDeleteFromObjectByKey(object, key){
-  
+ }
+ function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
 }
