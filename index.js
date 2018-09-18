470d43b73322824786a6588dbb30295d4aadd089
Ana Bebic <anabebic.x@gmail.com>
2018-09-18 18:36:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes= {};
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+ 
+  return Object.assign({}, obj, { [key]: value })
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+};
+
+function deleteFromObjectByKey(object, key){
+ var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
+};
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
