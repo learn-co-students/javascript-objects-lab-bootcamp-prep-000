24eb6a0e0ffbfec1139aee200f08aed21f6e145d
william george <williameverettgeorge@icloud.com>
2018-09-19 23:42:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = {};
+function updateObjectWithKeyAndValue (object, key, value){
+  object[key]=value
+return object
+}
+
+function updateObjectWithKeyAndValue (object, key, value){
+  
+}
