1bbfed5b74147ad51707687e6c14797449d80a5d
Brian <briandsalemi@gmail.com>
2018-09-21 20:55:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 var recipes = {}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { object[key] = value;
+return object }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return assign.Object({}, object, {[key]: value});
+}
