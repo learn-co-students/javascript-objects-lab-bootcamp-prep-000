22819fd0a8194d4fc0f8353961220ca95ab92980
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-21 23:31:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  recipes['key'] = 'value'
+  return recipes
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
