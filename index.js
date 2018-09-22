4b13064c49acf9774c0e94da89165615bbf587ae
dwolfe1987 <dwolfe1987@gmail.com>
2018-09-22 19:48:48 +0000

    Done.

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {object.
   
 }
