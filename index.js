eb37becc2c3800500dbb7be7b93534e2bdb820dd
william george <williameverettgeorge@icloud.com>
2018-09-23 23:47:53 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ function updateObjectWithKeyAndValue (object, key, value){
   object[key]=value
 return object
 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
 
-function updateObjectWithKeyAndValue (object, key, value){
-  
+ function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+delete object[key]
+return object
+}
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
 }
