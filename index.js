e576924ec4897b8050650758739bbf4b62d7879f
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-25 05:13:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 function updateObjectWithKeyAndValue(object,key,value){ return  Object.assign({},object,{[key]:value});}
-function destructivelyUpdateObjectWithKeyAndValue(object,key,value){ Object.assign(object,{[key]: value});}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){ Object.assign(object,{[key]: value}); return object;}
 var recipes={};
+function deleteFromObjectByKey(object,key){var copy=Object.assign({},object);delete copy[key]; return copy;}
+function destructivelyDeleteFromObjectByKey(object,key){delete object[key]; return object}
