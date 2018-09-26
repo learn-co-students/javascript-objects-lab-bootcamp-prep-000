7c0262a968704a429e7b89d4b377731ac7c77c95
Trey <tbeau5595@hotmail.com>
2018-09-26 19:26:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {
+  breakfast: "Eggs"
+};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({ [key]: value }, object);
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+ return delete object[key];
+ 
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+console.log(recipes);
