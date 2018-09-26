70824d1c7b51d9ab61d90e0243c2b8cbbad32ea4
jonroger3 <jonroger3@gmail.com>
2018-09-26 22:04:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+var Objects = recipes();
+
+function updateObjectWithKeyAndValue(obj, key, value){
+  
+  var Objects = { prop: 1 };
+  return Objects;
+  
+}
+
+updateObjectWithKeyAndValue(Objects, prop2, 2);
+
+
+destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  
+}
