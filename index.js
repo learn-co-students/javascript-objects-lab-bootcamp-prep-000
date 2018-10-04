d5f87e8045cdda5135a172aeca7f19ad75610fa3
Spencer D Stone <doomydoomdomm@gmail.com>
2018-10-04 00:21:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = new Object ({});
+
+//const obj = {prop: 1}
+function updateObjectWithKeyAndValue(object, key, value){
+  return recipes.assign({}, obj, { [key]: value});
+  return recipes
+
+}
+
+//function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+//  object[key] = value;
+  //return object
+//}
+
+//function deleteFromObjectByKey(object, key){
+  //delete object.key;
+  //return obj
+//}
