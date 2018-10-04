56469dfea72f57275256e2ab4760983fdebcaca5
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-04 01:47:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-   Object.entries(recipes);
-   var newRecipes = recipes[key] = value;
-   return;
+const newRecipes = Object.assign({},object);
+newRecipes[key] = value;
+return newRecipes;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+object[key] = value;
+return object; 
+}
+
+function deleteFromObjectByKey(object, key) {
+const newObject = Object.assign({},object);
+return delete newObject[key];
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key];
 }
