eef87798ecf02f22c85d3c2c2d69430cd9081b42
Rachel Lum <rlum894@gmail.com>
2018-10-10 22:02:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue (object, key, value) {
+  return Object.assign ({}, object, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+ object[key] = value
+ return object
+}
+
+function deleteFromObjectByKey (object, key) {
+  var newObj = Object.assign ({}, object.key)
+  delete newObj.key
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey (object, key){
+  //var object = {}
+  delete object[key]
+  return object
+}
