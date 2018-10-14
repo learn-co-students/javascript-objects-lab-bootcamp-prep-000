6a2810fcd58c9e0176196e609525ec982b271cd7
Jonathan Koss <jonathan_koss@yahoo.com>
2018-10-14 02:43:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-var recipes = new Object()
+var recipes = {prop: 1};
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
+function updateObjectWithKeyAndValue(recipes, key, value) {
+  return Object.assign({}, recipes, { [key]: value });
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  
-  return object
+  object[key] = value;
+  return object;
 }
 
-function deleteFromObjectByKey(object, key) {
-  return delete object.key
+function deleteFromObjectByKey(recipes, key) {
+  var newObj = Object.assign({}, recipes);
+  return delete newObj[key];
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
- return delete object[key]
+function destructivelyDeleteFromObjectByKey(recipes, key) {
+  return delete recipes[key];
 }
+/*
+  describe('deleteFromObjectByKey(object, key)', function() {
+    it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+      var newObj = deleteFromObjectByKey(obj, 'prop')
+
+      expect(newObj['prop']).toBe(undefined)
+    })
+
+    it('does not modify the original object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+
+      deleteFromObjectByKey(obj, 'prop')
+      expect(obj['prop']).toBe(1)
+    })
+  })
+
+  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
+    it('returns object without the delete key/value pair', function() {
+      var obj = { prop: 1 }
+      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+      expect(newObj['prop']).toBe(undefined)
+    })
+
+    it('modifies the original object', function() {
+      var obj = { prop: 1 }
+      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+      expect(obj['prop']).toBe(undefined)
+    })
+  })
+
+})
+
+*/
