bc23797d0918c6dcbec875477e1553e4566e1cba
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-16 18:29:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+const recipes = {};
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newObject = Object.assign({}, obj)
+  delete newObject[key]
+  return newObject
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key]
+  return obj
+}
