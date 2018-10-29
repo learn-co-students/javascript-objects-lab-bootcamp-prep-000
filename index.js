55a8b2dceb20e4db47b51a06c78af6528f66ba7b
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-10-29 17:44:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {Pizza: 'pepperoni'}
+function updateObjectWithKeyAndValue (object,key,value) {
+  return Object.assign( {},object, {[key]: value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  return Object.assign(object, {[key]: value})
+}
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({},object)
+  delete newObj[key]
+  return newObj
+}
+function destructivelyDeleteFromObjectByKey (object,key) {
+    delete object[key]
+  return object
+}
