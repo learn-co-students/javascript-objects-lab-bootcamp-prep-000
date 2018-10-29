2a898d695e4a9f3ff416c73982ff32b3c6860def
Brooke Gill <brooke.gill25@gmail.com>
2018-10-29 17:48:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = new Object({});
+
+var recipes = { prop: 1 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign( {}, object, {[key]: value} );
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newRecipes = Object.assign( {}, recipes )
+  delete newRecipes[key];
+  return newRecipes;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
