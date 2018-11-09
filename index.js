963b9eff4c6c9a9aad14241e361d428a35bcca03
Carlo Bruno <carlofbruno@gmail.com>
2018-11-09 06:04:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
-}
-
-function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]:value }); // shorthand
   
   // var newObj = Object.assign({}, object);
