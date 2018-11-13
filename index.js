fe2d3065185978c4333429f55fb15d593d80ac4c
Naenae L <oneprincesslea@gmail.com>
2018-11-13 18:13:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var recipes = { quiche: 'eggs' }
-var updateObjectWithKeyAndValue = Object.assign({}, recipes)
-updateObjectWithKeyAndValue
-console.log(updateObjectWithKeyAndValue);
-delete updateObjectWithKeyAndValue.quiche ;
-updateObjectWithKeyAndValue;
- console.log(updateObjectWithKeyAndValue);
+
+function updateObjectWithKeyAndValue (object, key, value){
+//  return Object.assign({}, recipes, {pancakes: "flour"});
+  return Object.assign ({}, recipes, {prop: 1});
+}
+updateObjectWithKeyAndValue ();
+console.log(updateObjectWithKeyAndValue());
 recipes;
 console.log(recipes);
+
+function destructivelyUpdateOjectWithKeyAndValue (object, key, value) {
+  return Object.assign({}, recipes, {pancakes: "flour"});
+//  return delete recipes.quiche;
+};
+console.log(destructivelyUpdateOjectWithKeyAndValue());
+function deleteFromObjectByKey (object, key){
+  return delete recipes.quiche;
+};
+console.log(deleteFromObjectByKey());
