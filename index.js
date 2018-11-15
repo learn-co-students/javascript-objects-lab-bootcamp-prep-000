4c6461cb121e2dc68f8d5c954e3b436faf06d060
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-15 16:25:12 +0000

    Done.

--- index.js
+++ index.js
@@ -16,24 +16,12 @@ function deleteFromObjectByKey(object, key) {
  return delete newObject[key];
 }
 
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key];
+}
+
 /*
- describe('deleteFromObjectByKey(object, key)', function() {
-    it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
-      var obj = { prop: 1 }
-      var newObj = deleteFromObjectByKey(obj, 'prop')
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('does not modify the original object (it is non-destructive)', function() {
-      var obj = { prop: 1 }
-
-      deleteFromObjectByKey(obj, 'prop')
-      expect(obj['prop']).toBe(1)
-    })
-  })
-
-  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
+ describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
     it('returns object without the delete key/value pair', function() {
       var obj = { prop: 1 }
       var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
