559735e6fe9ab08d6233ff487fe2143585468b2d
Jonathan Koss <jonathan_koss@yahoo.com>
2018-11-26 04:41:00 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,77 @@ var recipes = {
   turkeySandwich: ["Bread", "Turkey", "Mayonnaise"]
 };
 
+function updateObjectWithKeyAndValue(obj, key, value) {
+ return Object.assign({}, obj, {[key]: value});
+}
 
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj);
+  return delete newObj[key];
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  return delete obj[key];
+}
 
 /*
- describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
+describe('updateObjectWithKeyAndValue(object, key, value)', function() {
+    it('returns an object with the orignal key value pairs and the new key value pair', function() {
+      var obj = { prop: 1 }
+
+      expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+
+    it('it does not modify the original object, but rather returns a clone with the new data', function() {
+      var obj = { prop: 1 }
+
+      updateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+      expect(obj['prop2']).toBe(undefined)
+    })
+  })
+
+  describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
+    it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
+      var obj = { prop: 1 }
+
+      expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+
+      expect(obj).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+  })
+
+  describe('deleteFromObjectByKey(object, key)', function() {
+    it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+      var newObj = deleteFromObjectByKey(obj, 'prop')
+
+      expect(newObj['prop']).toBe(undefined)
+    })
+
+    it('does not modify the original object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+
+      deleteFromObjectByKey(obj, 'prop')
+      expect(obj['prop']).toBe(1)
+    })
+  })
+
+  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
     it('returns object without the delete key/value pair', function() {
       var obj = { prop: 1 }
       var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
@@ -22,4 +89,5 @@ var recipes = {
   })
 
 })
+
 */
