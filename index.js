e44bb2a84f758182b4b90cacb3631afe2175aafd
Paul Baluyot <zero4732@yahoo.com>
2018-11-27 05:55:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key) {
+  var boaz = Object.assign({}, object);
+  delete boaz[key];
+  return boaz;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
