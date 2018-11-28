d2cb8893a6b9f40fc3b70b765577e11092142834
Elaina <elainaseki@gmail.com>
2018-11-28 04:56:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+var obj = { prop: 1 };
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj,{[key]:value});
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ object[key] = value;
+ return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var newobj = object[key];
+   delete newobj[key];
+   return newobj;
+}
+
+ function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
