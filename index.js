6f5cb3137dfc221498cf451232c86867348607d9
Isabella de Medici <bellamedici242@gmail.com>
2018-12-01 20:08:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = new Object({})
+
+var obj = { prop: 1 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj, { [key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object
+}
+
+
+
