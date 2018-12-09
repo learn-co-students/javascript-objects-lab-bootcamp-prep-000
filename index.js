71ea51a0fbd8d1ca4449abd471b00558d1eb238e
Lerena Holloway <holloway.lerena@gmail.com>
2018-12-09 23:06:01 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
+function deleteFromObjectByKey(object, key){
+  delete object.key;
+}
+function destructivelyDeleteFromObjectByKey(object, prop){
+  delete object[prop];
+}
