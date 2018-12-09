6a74ac46ceb5525b7b03ebda4a6b1427cae14551
Lerena Holloway <holloway.lerena@gmail.com>
2018-12-09 22:57:57 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   var newObj = Object.assign({}, object)
   return newObj
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
