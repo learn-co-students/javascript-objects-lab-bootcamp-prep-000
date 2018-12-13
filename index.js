8b8930a8f1793b9f141e8345fa97a32cb4546287
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-12-13 01:29:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var recipes = {}; 
+var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return updateObjectWithKeyAndValue;
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
 }
-  
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  return newObj;
+}
+
