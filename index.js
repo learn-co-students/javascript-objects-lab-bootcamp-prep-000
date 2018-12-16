c5096c811f9a56edd45eb7b33236cc80d7d979bf
Jelena Klenkar <jelena.klenkar@gmail.com>
2018-12-16 19:25:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function objects(){
-  var objects = {recipes};
+var recipes = new Object ({});
+
+var obj = { prop: 1 };
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj,{[key]:value});
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+   object[key]=value;
+   return object;
+  
+}
+function deleteFromObjectByKey(object, key){
+  var newObj =  Object.assign({},object);
+  delete newObj[key];
+  return newObj;
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
 }
