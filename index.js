4534084f4c7a10114b3e5008050042485a5820d1
Ethan <ethansagin@gmail.com>
2018-12-20 21:49:10 +0000

    Done.

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 destructivelyUpdateObjectWithKeyAndValue(recipes, 'anothertypeof', 'anotherrecipe')
 
 var recipes = {typeof: 'recipe'}
-var newObject = Object.assign({}, recipes)
 function deleteFromObjectByKey(object, key){
-  delete newObject.key
+  const newObject = Object.assign({}, object)
+  delete newObject[key]
   return newObject
 }
-deleteFromObjectByKey(newObject, 'typeof')
+deleteFromObjectByKey(recipes, 'typeof')
 
 var recipes = {typeof: 'recipe'}
 function destructivelyDeleteFromObjectByKey(object, key){
