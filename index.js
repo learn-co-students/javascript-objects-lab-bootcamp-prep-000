646e347e085c801f69ee3862b4ff6b64a60f71f5
Jonathan Koss <jonathan_koss@yahoo.com>
2018-12-28 04:16:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 let recipes = {
-  "Turkey Sandwich": ["Bread", "Turkey", "Mayonnaise", "Lettuce"]
+  sandwich: ['Turkey', 'Bread', 'Lettuce', 'Mayo']
 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-
-  return Object.assign({}, object, {[key]: value});
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
-}
-
-function deleteFromObjectByKey(object, key) {
-  let newObj = Object.assign({}, object);
-  return delete newObj[key];
-}
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete object[key];
   
 }
 
