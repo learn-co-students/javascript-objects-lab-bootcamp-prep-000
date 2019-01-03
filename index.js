7f07c8c704f142ac700e39a904a8ab04fafe9ff0
Daniel Gorter <danieljgorter@gmail.com>
2019-01-03 20:21:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object,{[key]:value})
+}
+
+function deleteFromObjectByKey(object, key){
+  var tempObject = Object.assign({}, object)
+  delete tempObject[key]
+  return tempObject
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
