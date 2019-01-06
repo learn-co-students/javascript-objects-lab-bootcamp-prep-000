84599c28dfe82933cd5b746d09ef7731318a34c9
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-06 19:41:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(obj, key, value)
+  {
+    return Object.assign({}, obj, {[key]: value});
+  }
+  
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value)
+ {
+   obj[key] = value;
+   return obj;
+ }
+
+function deleteFromObjectByKey(object, key)
+  {
+   delete object.key;
+   return key;
+  }
+  
+function destructivelyDeleteFromObjectByKey(object, key)
+  {
+    delete object[key];
+    return object
+  }
