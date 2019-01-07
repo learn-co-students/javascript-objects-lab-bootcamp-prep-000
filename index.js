6b2ea62808170a1297cb1364d106ef4c9deacb3c
Brittany Malooly <bmalooly@gmail.com>
2019-01-07 03:52:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {
+  'breakfast': 'huevos rancheros',
+  'lunch': 'taquitos',
+  'dinner': 'enchiladas'
+}
+var newObj = { key, variable }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+ var newObj = Object.assign({}, object)
+ delete newObj[key]
+ return newObj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
