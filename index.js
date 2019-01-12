cbbed0fdd09540fb0e2732aba3395828bc0ec03f
Taher elshafie <t_elshafie10@yahoo.co.uk>
2019-01-12 17:05:28 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateObjectWithKeyAndValue(object,key,value)
 object[key]='value'
 return object
 }
-var obj = { prop: 1 }
-
+var obj={'prop1':'1'}
+updateObjectWithKeyAndValue(obj, 'prop2', 2)
 
