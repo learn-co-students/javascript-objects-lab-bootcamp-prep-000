bcd5f90fe11b1e621d441d7f3280d19a69a46750
Taher elshafie <t_elshafie10@yahoo.co.uk>
2019-01-12 17:08:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var recipes = {typeof:'object'}
-function updateObjectWithKeyAndValue(object,key,value)
+function updateObjectWithKeyAndValue(obj,key,value)
 {
-object[key]='value'
-return object
+obj[key]='value'
+return obj
 }
-var object={'prop1':'1'}
+var obj={'prop1':'1'}
 updateObjectWithKeyAndValue(obj, 'prop2', 2)
 
