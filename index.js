b25834f35fed8f4de455c2aedd20f4b1dbd08507
Daniel Schneider <daschne8@gmail.com>
2019-01-13 22:01:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function Objects(){
+  var recipes  = new Object();
+  return recipes;
+}
+function updateObjectWithKeyAndValue(object,key,value){
+  var newObject = Object.assign({},object);
+  newObject[key] = value;
+  return newObject;
+}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object,key){
+  var newObject = Object.assign({},object);
+  delete newObject[key];
+  return newObject;
+}
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key];
+  return object;
+}
