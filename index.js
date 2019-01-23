6451fa66fcb5f5a290daddbc2fde17fd9a38d3df
Matthew Shin <mshin1995@gmail.com>
2019-01-23 05:36:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {}
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  delete Object.assign({}, object[key])
+  return Object.assign({}, object[key])
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
