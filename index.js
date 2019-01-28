82cefaff1f5b2f3c5cf311fdc555cf070d42c77d
ko kristin <koberlander115@gmail.com>
2019-01-28 14:45:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var recipes = [prop: 1, prop2: 2];
+var recipes = {'prop': '1', 'prop2': '2'};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  // returns an object with the orignal key value pairs and the new key value pair
-  Object.assign({}, {'prop2': '2'})
-  return object;
+  return Object.assign({}, object, { [key]: value } )
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
@@ -19,6 +17,6 @@ function deleteFromObjectByKey(object, key){
 function destructivelyDeleteFromObjectByKey(object, key){
   // returns object without the delete key/value pair
   // modifies the original object
-  
-  return ;
+  delete object.prop
+  return object;
 }
