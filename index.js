97713f4fb8634c62d298f9e816637876afe05499
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-02 16:27:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+let recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newRecipe = Object.assign({}, object)
+  delete newRecipe[key]
+  return newRecipe
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
