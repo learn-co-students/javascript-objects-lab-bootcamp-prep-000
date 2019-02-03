106cadf5630db90ea0d96fb5884999ef0527d2db
Rufus <ruufman@gmail.com>
2019-02-03 04:37:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+
+function deleteFromObjectByKey(object, key) {
+  var login = Object.assign({}, object);
+  delete login[key];
+  return login;
+}
+
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
