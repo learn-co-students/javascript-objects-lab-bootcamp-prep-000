24de52899837f87c8dbda6746596fef3b4e371b0
Diana Utan <dia_utzi@yahoo.com>
2019-02-04 19:38:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject= Object.assign({}, object, {[key] : value});
-  // newObject[key] = value;
+function updateObjectWithKeyAndValue (object, key, value) {
+  var newObject =  Object.assign({}, object, {[key]: value});
   return newObject;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+ object[key] = value;
+ return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({}, object)
+  var newObject = Object.assign({}, object);
   delete newObject[key];
   return newObject;
 }
-
-function destructivelyDeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey (object, key) {
   delete object[key];
   return object;
 }
