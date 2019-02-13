011803d32b673507642304e2e02e676b94c2363e
Ranae Rodgers <ranae.rodgers@icloud.com>
2019-02-13 23:05:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-var recipes = {};
+var recipes = {}
+
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
+  return Object.assign({}, object, { [key]: value })
 }
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  object[key] = value
+
+  return object
 }
-function deleteFromObjectByKey(object, key, value) {
-  let recipes = Object.assign({}, object, {[key]: value});
-  delete recipes[key];
-  return recipes;
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
+
+  delete newObj[key]
+
+  return newObj
 }
-function destructivelyDeleteFromObjectByKey(object, key, value) {
-  delete recipes[key];
-  return key;
-}
-function destructivelyDeleteFromObjectByKey(object, key, value) {
-  object[key] = value;
-  return object;
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+
+  return object
 }
