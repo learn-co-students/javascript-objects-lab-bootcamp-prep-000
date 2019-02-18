72e08361bd9dbe77d5cd37cb03f96b8a8f9c2511
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-02-18 21:52:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var recipes = {}
+var recipes = {};
 
-function updateObjectWithKeyAndValue(object,key,value){
-  return object.assign({},{key: value})
+function updateObjectWithKeyAndValue(obj1,key1,val1){
+  var newObj = {};
+  Object.assign(newObj,obj1,{[key1]:val1})
+  return newObj
 }
