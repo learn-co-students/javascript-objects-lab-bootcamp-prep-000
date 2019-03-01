5672b48a18dd68273f26100e651c9d7327b1c1da
Christopher Cabell <cccabell@icloud.com>
2019-03-01 23:33:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-var recipes = {}
+var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign({}, obj, {[key]: value})
+  return Object.assign({}, obj, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {obj[key] = value
-  return obj
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {obj[key] = value;
+  return obj;
 }
 
 function deleteFromObjectByKey(obj, key) {
-  var temp = Object.assign({},obj)
-  delete temp[key]
-  return temp
+  var temp = Object.assign({},obj);
+  delete temp[key];
+  return temp;
 }
 
 function destructivelyDeleteFromObjectByKey(obj, key) {
-  delete obj[key]
-  return obj
+  delete obj[key];
+  return obj;
 }
