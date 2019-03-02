96dca56ab5d06d93125e4e43a3a35074116a52f4
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-02 15:32:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = {key: 'value'};
+
+function updateObjectWithKeyAndValue(object, key, value){
+return Object.assign({},object,{[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object [key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  newObj;
+}
