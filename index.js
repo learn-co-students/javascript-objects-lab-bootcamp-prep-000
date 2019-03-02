b432da99c72a058a74fb75070c130bf11b213f2e
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-02 16:16:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ return Object.assign({},object,{[key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object [key] = value;
+  object[key] = value;
   return object
 }
 
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object);
-  delete newObj.key;
-  newObj;
+  delete newObj[key];
+ return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object [key]
+  return object
 }
