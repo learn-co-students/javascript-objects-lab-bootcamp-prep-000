64b696a04c83c8ebdb60756085f90e1b7e0e39cc
Nicholas Stano <nicholasstano@gmail.com>
2019-03-04 19:44:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-var recipes = {}
+var recipes = { }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
+  
+  return Object.assign({}, object, {[key]:value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-object[key] = value
-return object
+  object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object, key) {
+ 
   var newObj = Object.assign({}, object)
-  delete newObj[key] 
+  delete newObj[key]
   return newObj
 }
 
