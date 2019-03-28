48f87ea34d7841ec4f5e234fb82c03975f3ac314
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-03-28 14:20:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {};
+
+function  updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+    return Object.assign(object, {[key]: value});
+}
+function deleteFromObjectByKey(object, key){
+  let obj = Object.assign({}, object)
+  delete obj[key]
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  return delete object[key];
+}
