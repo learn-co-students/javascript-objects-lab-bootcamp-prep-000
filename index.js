997f6227691b082d59788b94f8ef2c87853de905
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-04-09 05:16:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},object,{[key]:value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key) {
+  var object1;
+  object1 = Object.assign({},object);
+  delete object1[key];
+  return object1;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
