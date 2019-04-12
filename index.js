acf877532961644bfd92a7b5bccea91f57b9134c
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 03:48:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newRecipe = Object.assign({},object)
+  newRecipe[key] = value
+  return newRecipe
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  var newRecipe = Object.assign({},object)
+  delete newRecipe[key]
+  return newRecipe
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
