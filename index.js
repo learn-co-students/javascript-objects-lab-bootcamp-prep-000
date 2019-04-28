9b4cfd2f5bbc1a9ec6e8bb75b3299c67da607e1a
modern-channel-7516 <svetlana.rovinsky@gmail.com>
2019-04-28 23:04:43 +0000

    Done.

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 //Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and re
 //turns the new object (it is non-destructive):
 function deleteFromObjectByKey(object, key){
-  let copyObj = object;
-  delete copyObj[key];
-  return copyObj;
+let newObj = Object.assign({}, object);
+delete newObj[key];
+return newObj;
+}
+
+//Objects destructivelyDeleteFromObjectByKey(object, key) returns object without the delete key/value pair:
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
 }
