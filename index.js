a046066efe933e8c8cc8c68ca7193726671bf6a4
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-28 19:40:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var recipes = {spices:"paprika"}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  recipes["spices"]="paprika"
+  return recipes
+}
+
+function updateObjectWithKeyAndValue(object,key,value){
+  return Object.assign({},object,{[key] : value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key, value){
+ object[key] = value
+
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+var newObj = Object.assign({}, object)
+delete newObj[key];
+return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+delete object[key];
+return object
+}
+
