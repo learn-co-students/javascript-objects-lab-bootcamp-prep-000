8f143e0181c21aa823cc8a9c273e71b4722cb7a2
Anais Trope <trope.anais@gmail.com>
2019-04-30 13:33:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes={}
 
 function updateObjectWithKeyAndValue(object, key, value){
- var newObj = Object.assign({}, object,{[key]: value})
+var newObj = Object.assign({}, object,{[key]: value})
  return newObj;
 }
 
