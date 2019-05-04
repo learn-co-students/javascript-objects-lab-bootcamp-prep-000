28ae810f92b9a144a14614d24f17ae1b4fe42550
salubrious-reference-8531 <philippetimothe@gmail.com>
2019-05-04 20:51:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var recipes = {
+  
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  delete object[key];
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  return Object.assign({}, object[key])
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
+ 
+
