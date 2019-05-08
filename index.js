3deb53f5a8a34f6a31fe2b249047645ee316156d
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-08 02:58:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+let recipes = {
+  'Egg Sandwich': ['Eggs', 'Mayo', 'Salt', 'Pepper', 'Breas']
+};
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  let newObject = Object.assign({}, object);
+  return delete newObject[key];
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key];
+}
 
 
