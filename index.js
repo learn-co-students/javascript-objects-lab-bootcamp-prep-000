368ad5cee6f2f71fc6608cb144b39b97bc1ff99f
Adam Brown <adam.brown.betty@gmail.com>
2019-07-01 03:03:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
+/*  run test to see what it wants
+create an object called recipes
+use Object.assign to make a new object that has the original K-V pair, and the the new ones
+*/
+
 var recipes = {key: 'value'};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   
-  return Object.assign({}, object, {[key] : value})
+  return Object.assign({}, object, {[key] : value});
 }
 
+/* update the existing object and return the updated one.
+*/
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   
     return object;
 }
 
+/* create a new object to store 
+
+*/
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete newObj[key]
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
   return newObj;
 }
 
+/*
+
+*/
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
+  delete object[key];
   
     return object;
 }
