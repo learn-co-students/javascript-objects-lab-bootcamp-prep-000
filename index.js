ce9fc886f3571aa41abcc4aed65d7d5456a9837a
Bello Osagie <osagienoah04@gmail.com>
2019-07-06 20:15:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var recipes = {prop: 1}
+var recipes = {prop: 1};
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value })
 }
-updateObjectWithKeyAndValue(recipes, 'prop2', 2)
+updateObjectWithKeyAndValue(recipes, 'prop2', 2) // {prop: 1}
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
 }
-destructivelyUpdateObjectWithKeyAndValue(recipes, 'prop2', 2)
+destructivelyUpdateObjectWithKeyAndValue(recipes, 'prop2', 2) // { prop: 1, prop2: 2 }
 
-function deleteFromObjectByKey(object, key, value){
-  return delete Object.assign({}, object, { [key]: value })
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj[key];
+  return newObj;
 }
-deleteFromObjectByKey(recipes, 'prop', 1)
+deleteFromObjectByKey(recipes, 'prop') // { prop2: 2 }
 
-function destructivelyDeleteFromObjectByKey(object, key, value){
-  object[key] = value
-  return delete object.key
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
 }
-destructivelyDeleteFromObjectByKey(recipes, 'prop', 1)
+destructivelyDeleteFromObjectByKey(recipes, 'prop') // true
 
 /* // updateObjectWithKeyAndValue
 var recipes = {prop: 1}
