d0814983bdda19cce319e49e1219d3ed89269ccc
Bello Osagie <osagienoah04@gmail.com>
2019-07-06 15:23:18 +0000

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,23 @@ function destructivelyDeleteFromObjectByKey(object, key, value){
   return delete object.key
 }
 destructivelyDeleteFromObjectByKey(recipes, 'prop', 1)
+
+/* // updateObjectWithKeyAndValue
+var recipes = {prop: 1}
+console.log(Object.assign({}, recipes, { ['prop2']: 2 })) // { prop: 1, prop2: 2 }
+console.log(recipes) // { prop: 1 } */
+
+/* // destructivelyUpdateObjectWithKeyAndValue
+var recipes = {prop: 1};
+recipes['prop2'] = 2
+console.log(recipes) // { prop: 1, prop2: 2 } */
+
+/* // deleteFromObjectByKey
+var recipes = {prop: 1};
+console.log(delete Object.assign({}, recipes, { ['prop']: 1 })) // true
+console.log(recipes) // { prop: 1 } */
+
+/* // destructivelyDeleteFromObjectByKey
+var recipes = {prop: 1}; // recipes['prop'] = 1
+console.log(delete recipes.prop)
+console.log(recipes) // {} */
