2ae61f46651af367d6c453f367f012e38c578aef
Bello Osagie <osagienoah04@gmail.com>
2019-07-06 20:39:26 +0000

    Done.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 updateObjectWithKeyAndValue(recipes, 'prop2', 2) // {prop: 1}
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  var newObj = bject[key] = value;
-  return newObj;
+  object[key] = value;
+  return object;
 }
 destructivelyUpdateObjectWithKeyAndValue(recipes, 'prop2', 2) // { prop: 1, prop2: 2 }
 
