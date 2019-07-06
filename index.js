e5c3063e129fee81121cfc793a153fea5a182b3d
Bello Osagie <osagienoah04@gmail.com>
2019-07-06 11:52:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {prop: 1}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+updateObjectWithKeyAndValue(recipes, 'prop2', 2)
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+updateObjectWithKeyAndValue(recipes, 'prop2', 2)
+
+function deleteFromObjectByKey(object, key, value){
+  return delete Object.assign({}, object, { [key]: value })
+}
+deleteFromObjectByKey(recipes, 'prop', 1)
+
+function destructivelyDeleteFromObjectByKey(object, key, value){
+  object[key] = value
+  return delete object.key
+}
+destructivelyDeleteFromObjectByKey(recipes, 'prop', 1)
