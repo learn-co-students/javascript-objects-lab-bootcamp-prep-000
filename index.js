433ed7259a689b6c88c50b7037ae4d1b2483f673
Arlene George <aemg@live.com>
2019-07-14 19:05:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {oatmeal: "milk"};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+ var recipes = {prop: '1', 
+   prop2: '2'};
+ return recipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+   object[key] = value
+   return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete recipes.prop
+  return recipes
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+return object
+}
