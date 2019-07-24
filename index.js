eb92d7d9fb7871ef22ed80cc4a09e10abdccd347
fortuitous-map-5512 <danielionita88@yahoo.com>
2019-07-24 19:14:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  var newObj = Object.assign({}, obj, {[key]: value});
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
