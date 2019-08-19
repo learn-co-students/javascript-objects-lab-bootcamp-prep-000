f3d7d514044ca77baca26827ccc8911078aa50f6
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-19 23:33:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = new Object()
+
+function updateObjectWithKeyAndValue(recipes, key, value) {
+  return Object.assign({}, recipes, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key] = value
+  return recipes
+}
+
+function deleteFromObjectByKey(recipes, key) {
+  var newRecipes = Object.assign({}, recipes)
+  delete newRecipes[key]
+  return newRecipes
+}
+
+function destructivelyDeleteFromObjectByKey(recipes, key) {
+  delete recipes[key]
+  return recipes
+}
