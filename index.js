7c9c75dc6cc46918e09f3ae098b0572ed543dae8
free-writer-5650 <miles.higbie@gmail.com>
2019-10-07 00:35:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+const recipes = {}; 
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},object,{[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object, key){
+  let newObj = Object.assign({},key);
+  delete newObj[key];
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
