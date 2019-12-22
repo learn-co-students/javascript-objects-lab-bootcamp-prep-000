264e63d8550792322f9fdecb484ac86fec877982
Benjamin Horns <benhorns@gmail.com>
2019-12-22 17:17:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function updateObjectWithKeyAndValue (object, key, value){
+return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object [key] = value
+  return object
+}
+
+function deleteFromObjectByKey (object, key){
+  var object = { key: 'bar' }
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
