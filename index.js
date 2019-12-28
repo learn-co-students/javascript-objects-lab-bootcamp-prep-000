0420d87c357a8cbb74b282fd45d30a306ec3558a
Marcelo Martinez <marcelomst1@gmail.com>
2019-12-28 19:25:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object,{[key]: value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+   return Object.assign(object,{[key]: value});
+}
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({},object)
+  delete newObj[[key]]
+  return newObj 
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[[key]]
+  return object
+}
