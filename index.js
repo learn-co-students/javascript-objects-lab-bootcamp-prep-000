65869e1e6690615e9fbb3950b459c07f9994555d
zaid85j <zaid85j@gmail.com>
2020-03-28 15:20:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 function updateObjectWithKeyAndValue(object, key, value){
+
   return Object.assign({},object,{[key]:value})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
@@ -8,4 +9,4 @@ return object
 } 
 function deleteFromObjectByKey(object, key){
   return delete object[key]
-}
+ }
