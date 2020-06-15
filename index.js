bb22d08b2d8496e23a44be948e860cb600c7e8e2
Cedric Patton <ceddyp117@gmail.com>
2020-06-15 03:38:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function UpdateObjectWithKeyAndValue(Object, key, value){
+  Object.assign(obj = {prop2: 2 });
+  return obj;
+}
+
+
+
+
+function deleteFromObjectByKey(obj, key)
+
+
+
+function destructivelyDeleteFromObjectByKey(obj, key){
+
+delete obj.prop;
+return obj;
+}
