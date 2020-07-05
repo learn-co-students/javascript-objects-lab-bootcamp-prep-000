702ad20f2b9006ae1f6d1c455ea5ed31afe7a4d5
Christopher Ripley <clripley@gmail.com>
2020-07-05 23:49:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function updateObjectWithKeyAndValue(object, key, value){
+   return Object.assign({}, object, {[key]: value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  var obj = {object}
+  var newObj = Object.assign({}, obj)
+  delete newObj.object
+  return newObj
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+delete object[key]
+return object
+}
