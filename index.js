d4d70be2cf6bba76f5191c0fdc647aafd6c5d89a
Angelo Milien <angelomilienjoseph@gmail.com>
2020-08-10 20:02:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+var recipes = {
+  prop: 1
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+    recipes[key] = value
+    return recipes
+ }
+ 
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    object[key] = value
+    return object
+ }
+ 
+ function deleteFromObjectByKey(object, key) {
+   var newObj = Object.assign({}, recipes)
+   delete newObj.prop
+   return newObj
+  
+ }
+ 
+ 
+ function destructivelyDeleteFromObjectByKey(object, key) {
+    delete recipes.prop
+    return recipes
+   
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
