48553373277e181cc9900b4993a84ece713d78a7
blobbyblobfish <aywang6@gmail.com>
2020-08-24 22:10:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+// Object.assign creates a new object with given object and key/values
 function updateObjectWithKeyAndValue(object, key, value) {
     let newKeyValue = {}
     newKeyValue[key] = value
     return Object.assign({}, object, newKeyValue)
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-    object[key] = value
-    return object
-}
-
 function deleteFromObjectByKey(object, key) {
     const newObj = Object.assign({}, object)
     delete newObj[key]
     return newObj
+}
+
+// Bracket notation allows variables to be used as object keys
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    object[key] = value
+    return object
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
