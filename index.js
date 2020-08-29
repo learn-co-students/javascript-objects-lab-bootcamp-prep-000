5051dace3ce2611431e4fbbe2c8782e03492b38a
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-29 01:35:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var recipes = {
+  flour: "2 cups",
+  sugar: "1tbs",
+  milk: "1 cup"
+}
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+  
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return(object)
+}  
+
+
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object)
+  delete clone[key] 
+  return(clone)
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return(object)
+}
+
