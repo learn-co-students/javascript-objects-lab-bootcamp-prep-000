a0a1573c16b9fed1d2e793c60e6671aec9761539
Sarah A <sarah.alhuriz@hotmail.com>
2020-09-05 22:29:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, object, {[key]: value});
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  Object.assign(object, {[key]: value});
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  newObj = delete newObj.key;
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
+
+
