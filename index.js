6613876bf8a6938fc60a122f2fdbed8bde28482b
Magdalena Schuetz <m.schuetz@outlook.de>
2020-09-06 08:58:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+var recipes = {};
+
+/* - returns an object with the orignal key value pairs and the new key value pair
+   -  does not modify the original object, but rather returns a clone with the new data
+   - returns an object with an updated key value pair */
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value });
+  }
+
+/* - updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object */
+
+  function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+  }
+
+/* - deletes `key` from a clone of object and returns the new object (it is non-destructive)
+   - does not modify the original object (it is non-destructive) */
+
+  function deleteFromObjectByKey(object, key) {
+    var newObj = Object.assign({}, object);
+    delete newObj[key];
+    return newObj;
+  }
+
+/* - does not modify the original object (it is non-destructive)
+   - modifies the original object */
+
+  function destructivelyDeleteFromObjectByKey(object, key) {
+   delete object[key];
+   return object;
+  }
