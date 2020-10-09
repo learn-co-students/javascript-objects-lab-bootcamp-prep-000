4ce83380569d8562cdc23f1aac355b1dbce89ffe
Nathan Rymarz <nrymarz@gmail.com>
2020-10-09 22:03:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+recipes = {}
+function updateObjectWithKeyAndValue(obj,key,value){
+  return Object.assign({},obj,{[key]:value});
+}
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
+  obj[key] = value;
+  return obj;
+}
+function deleteFromObjectByKey(obj,key){
+  var newobj = Object.assign({},obj);
+  delete newobj[key];
+  return newobj
+}
+function destructivelyDeleteFromObjectByKey(obj,key){
+  delete obj[key];
+  return obj;
+}
