7e34b000f995d6654e0f37d4483b7ee5b4d6af8b
Peter Crawford <pcraw1027@gmail.com>
2021-02-02 21:46:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function  destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
